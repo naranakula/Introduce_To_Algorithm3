64fe8b945dda87286350397117ea91b1e464712a
luchunminglu <luchunminglu@gmail.com>
2013-05-02 23:23:14 +0800

    heap

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Structs/MaxHeap.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Structs/MaxHeap.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Introduce_To_Algorithm3.Common.Sort;
 
 namespace Introduce_To_Algorithm3.Common.Structs
 {
     /// <summary>
-    /// the value of parent bigger than all his children
+    /// the value of parent >= all his children
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public class MaxHeap<T> where T:IComparable<T>
@@ -50,6 +51,15 @@ namespace Introduce_To_Algorithm3.Common.Structs
         }
 
         /// <summary>
+        /// get the index of first Leaf
+        /// </summary>
+        /// <returns></returns>
+        private int FirstLeaf()
+        {
+            return len/2;
+        }
+
+        /// <summary>
         /// get the capaticity of heap
         /// </summary>
         public int Capaticity
@@ -64,5 +74,63 @@ namespace Introduce_To_Algorithm3.Common.Structs
         {
             get { return len; }
         }
+
+        /// <summary>
+        /// lets the value at arr[i] “float down” in the max-heap so that the subtree rooted at index i obeys the max-heap property.
+        /// it runs at O(lgn)
+        /// </summary>
+        /// <param name="i"></param>
+        private void Sink(int i)
+        {
+            int left = Left(i);
+            int right = Right(i);
+            int max = i;
+            if(left<len && arr[left].CompareTo(arr[i])>0)
+            {
+                max = left;
+            }
+
+            if(right <len && arr[right].CompareTo(arr[i])>0)
+            {
+                max = right;
+            }
+
+            if(max != i)
+            {
+                SortCommons<T>.Exchange(arr,max,i);
+                Sink(max);
+            }
+        }
+
+        /// <summary>
+        /// build a heap in O(n)
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static MaxHeap<T> BuildMaxHeap(T[] source)
+        {
+            MaxHeap<T> heap = new MaxHeap<T>();
+            if(source == null || source.Length == 0)
+            {
+                heap.len = 0;
+                return heap;
+            }
+            if(source.Length == 1)
+            {
+                heap.arr[0] = source[0];
+                heap.len = source.Length;
+                return heap;
+            }
+            heap.len = source.Length;
+            heap.arr = source;
+
+            for(int i=heap.FirstLeaf()-1;i>=0;i--)
+            {
+                heap.Sink(i);
+            }
+            return heap;
+        }
+
+
     }
 }
