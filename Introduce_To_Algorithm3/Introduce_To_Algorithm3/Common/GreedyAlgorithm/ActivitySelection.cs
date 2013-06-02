52d47b3b86e20bcf7acd83839eb03a2430214625
luchunminglu <luchunminglu@gmail.com>
2013-06-02 22:35:55 +0800

    c

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/GreedyAlgorithm/ActivitySelection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Introduce_To_Algorithm3.Common.GreedyAlgorithm
+{
+    /// <summary>
+    /// we have activity set {a1,a2,.....,an},each activity ai has a start time si and a finish time fi.
+    /// In activity selection problem, we wish to select a maximum-size subset of mutually compatible activities.
+    /// Activity ai and aj are compatible if [si,fi) and [sj,fj) do not overlap
+    /// </summary>
+    public class ActivitySelection
+    {
+    }
+}
