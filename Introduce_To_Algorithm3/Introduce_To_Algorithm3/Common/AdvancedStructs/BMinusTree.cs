0019e5eb3dfcac5807fa0baf96fdd250fab081e5
luchunminglu <luchunminglu@gmail.com>
2013-06-05 23:21:10 +0800

    c

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/AdvancedStructs/BMinusTree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Introduce_To_Algorithm3.Common.AdvancedStructs
+{
+    /// <summary>
+    /// 1)every non root non leaf node has [M/2,M] child
+    /// 2)root has [2,M] child
+    /// 3)every leaf has same height
+    /// 4)every non root node has [M/2-1,M-1] keys
+    /// it normally used for db
+    /// </summary>
+    public class BMinusTree
+    {
+    }
+}
