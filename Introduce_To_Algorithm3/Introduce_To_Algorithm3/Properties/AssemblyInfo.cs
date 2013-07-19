9084f2a44ff5d375807bd3e706254d135ae69d67
chlu <luchunminglu@gmail.com>
2013-07-19 02:39:06 -0700

    c

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Properties/AssemblyInfo.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// 有关程序集的常规信息通过以下
+// 有关程序集的常规信息通过以下 
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Introduce_To_Algorithm3")]
