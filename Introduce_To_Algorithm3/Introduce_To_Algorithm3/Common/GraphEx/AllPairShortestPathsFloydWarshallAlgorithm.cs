a2b8350bd7608a0914021dbf35851c5bbcdd39bc
luchunminglu <luchunminglu@gmail.com>
2013-07-27 14:47:29 +0800

    floydwarshall

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/GraphEx/AllPairShortestPathsFloydWarshallAlgorithm.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/GraphEx/AllPairShortestPathsFloydWarshallAlgorithm.cs
@@ -7,5 +7,28 @@ namespace Introduce_To_Algorithm3.Common.GraphEx
 {
     public class AllPairShortestPathsFloydWarshallAlgorithm
     {
+        private double[,] weights;
+        public AllPairShortestPathsFloydWarshallAlgorithm(EdgeWeightedAdjMatrixDigraph g)
+        {
+            int n = g.V;
+            double[,] weis0 = g.Weights();
+            for (int k = 0; k < n; k++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        weis0[i, j] = System.Math.Min(weis0[i, j], weis0[i, k] + weis0[k,j]);
+                    }
+                }
+            }
+            weights = weis0;
+        }
+
+
+        public double[,] GetWeights()
+        {
+            return weights;
+        }
     }
 }
