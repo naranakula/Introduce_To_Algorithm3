eae9a67c18d18c47246c8f7ebb8368fe082de829
luchunminglu <luchunminglu@gmail.com>
2013-09-27 03:15:41 -0700

    c

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/Utils.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/Utils.cs
@@ -77,6 +77,102 @@ namespace Introduce_To_Algorithm3.Common.Utils
             return new FileInfo(file).Length;
         }
 
+        /// <summary>
+        /// mirror srcDir and tgtdir
+        /// </summary>
+        /// <param name="srcDir"></param>
+        /// <param name="searchPattern"></param>
+        /// <param name="tgtDir"></param>
+        public static void MirrorDir(string srcDir, string searchPattern, string tgtDir)
+        {
+            foreach (string file in RetryDirectory.Default.GetFiles(srcDir,searchPattern))
+            {
+                UpdateToDir(file, tgtDir);
+            }
+
+            foreach (string file in RetryDirectory.Default.GetFiles(tgtDir,searchPattern))
+            {
+                if(!RetryFile.Default.Exists(Path.Combine(srcDir,Path.GetFileName(file))))
+                {
+                    RetryFile.Default.Delete(file);
+                }
+            }
+        }
+
+        /// <summary>
+        /// mirror srcDir and tgtDir
+        /// </summary>
+        /// <param name="srcDir"></param>
+        /// <param name="tgtDir"></param>
+        public static void MirrorDir(string srcDir, string tgtDir)
+        {
+            MirrorDir(srcDir, "*", tgtDir);
+        }
+
+        /// <summary>
+        /// recursive mirror srcDir and tgtDir
+        /// </summary>
+        /// <param name="srcDir"></param>
+        /// <param name="tgtDir"></param>
+        public static void RecursiveMirrorDir(string srcDir, string tgtDir)
+        {
+            if (string.IsNullOrWhiteSpace(tgtDir))
+            {
+                return;
+            }
+
+            if (!RetryDirectory.Default.Exists(tgtDir))
+            {
+                RetryDirectory.Default.CreateDirectory(tgtDir);
+            }
+
+            foreach (string subSourceDir in RetryDirectory.Default.GetDirectories(srcDir))
+            {
+                RecursiveMirrorDir(subSourceDir, Path.Combine(tgtDir, Path.GetFileName(subSourceDir)));
+            }
+
+            MirrorDir(srcDir, tgtDir);
+        }
+
+        /// <summary>
+        /// recursive update to directory
+        /// </summary>
+        /// <param name="srcDir"></param>
+        /// <param name="searchPattern"></param>
+        /// <param name="tgtDir"></param>
+        public static void RecursiveUpdateToDir(string srcDir, string searchPattern, string tgtDir)
+        {
+            if (string.IsNullOrWhiteSpace(tgtDir))
+            {
+                return;
+            }
+
+            if (!RetryDirectory.Default.Exists(tgtDir))
+            {
+                RetryDirectory.Default.CreateDirectory(tgtDir);
+            }
+
+            foreach (string subSourceDir in RetryDirectory.Default.GetDirectories(srcDir))
+            {
+                RecursiveUpdateToDir(subSourceDir, searchPattern, Path.Combine(tgtDir, Path.GetFileName(subSourceDir)));
+            }
+
+            foreach (string srcFile in RetryDirectory.Default.GetFiles(srcDir,searchPattern))
+            {
+                UpdateToDir(srcFile, tgtDir);
+            }
+        }
+
+        /// <summary>
+        /// recursive update to directory
+        /// </summary>
+        /// <param name="srcDir"></param>
+        /// <param name="tgtDir"></param>
+        public static void RecursiveUpdateToDir(string srcDir, string tgtDir)
+        {
+            RecursiveUpdateToDir(srcDir, "*", tgtDir);
+        }
+
         #endregion
     }
 
