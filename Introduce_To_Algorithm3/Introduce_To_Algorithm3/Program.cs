e454723031d376b8c2b3013721498d650adb28e6
luchunminglu <luchunminglu@gmail.com>
2014-03-09 19:13:05 -0700

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Program.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Program.cs
@@ -15,6 +15,7 @@ using System.Threading;
 using Introduce_To_Algorithm3.Common.DynamicProgramming;
 using System.Net;
 using Introduce_To_Algorithm3.Common.Sort;
+using System.Threading.Tasks;
 
 namespace Introduce_To_Algorithm3
 {
@@ -22,120 +23,33 @@ namespace Introduce_To_Algorithm3
     {
         static void Main(string[] args)
         {
-            int codeCounter = CodeCounter.GetCodeLines(@"C:\Users\v-chlu\Documents\GitHub\Introduce_To_Algorithm3\Introduce_To_Algorithm3\Introduce_To_Algorithm3");
-            int limit = args.Length<=1?1000000: int.Parse(args[0]);
-            int m = 3;
             while (true)
             {
-                Random rand = new Random();
-                BTree<int, int> bTree = new BTree<int, int>(m++);
-                DateTime dt = DateTime.Now;
-                List<int> list = new List<int>();
-                for (int i = 0; i < limit; i++)
-                {
-                    int k = rand.Next();
-                    list.Add(k);
-                    bTree.Insert(k, k);
-                }
-                list = list.Distinct().ToList();
-                var array = list.ToArray();
-                QuickSort<int>.RandomRearrange(array);
-                list = array.ToList();
-                Console.WriteLine(DateTime.Now - dt);
-                if (bTree.IsBTree())
-                {
-                    Console.WriteLine("it is btree mindegree = "+(m-1)+"  hight="+bTree.Heigth+"  count="+bTree.Count+"  DateTime="+DateTime.Now);
-                }
-                else
-                {
-                    Console.WriteLine("not BTREE");
-                    Console.ReadLine();
-                }
-
-                foreach (var item in list)
-                {
-                    bTree.Delete(item);
-                    if (bTree.IsBTree())
-                    {
-                        Console.WriteLine("it is btree mindegree = " + (m - 1) + "  hight=" + bTree.Heigth + "  count=" + bTree.Count + "  DateTime=" + DateTime.Now);
-                    }
-                    else
-                    {
-                        Console.WriteLine("not BTREE");
-                        Console.ReadLine();
-                    }
-                }
-                
-
-                Console.WriteLine("==========================");
-                Console.WriteLine();
-                Console.WriteLine();
-                Thread.Sleep(10000);
-            }
-        }
-
-        static void MainProxy1(string[] args)
-        {
-            while (true)
-            {
-                string s = Console.ReadLine();
-                if (string.IsNullOrEmpty(s))
-                {
-                    continue;
-                }
-                Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-                IPEndPoint iPEndPoint = new IPEndPoint(Dns.GetHostAddresses("mt-dev-07")[0], 19999);
-                socket.Connect(iPEndPoint);
-                socket.Send(Encoding.Unicode.GetBytes(s));
-                byte[] buffer = new byte[512];
-                int readNum = socket.Receive(buffer, 0, buffer.Length, SocketFlags.None);
-                Console.WriteLine(Encoding.Unicode.GetString(buffer, 0, readNum) + "  >>> " + DateTime.Now);
-                socket.Shutdown(SocketShutdown.Both);
-                socket.Close();
-            }
-        }
-
-        static void MainProxy2(string[] args)
-        {
-            Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 19999);
-            socket.Bind(iPEndPoint);
-            socket.Listen(1024);
-            while (true)
-            {
-                if (!socket.Poll(100000, SelectMode.SelectRead))
+                string line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
                 {
                     continue;
                 }
 
-                Socket client = socket.Accept();
+                if (line == "stop")
+                {
+                    return;
+                }
+                TcpClientEx tcpclient = new TcpClientEx("172.31.25.34", 1999);
+                tcpclient.Connect();
+                var client = tcpclient.GetTcpClient();
 
-                byte[] buffer = new byte[512];
-                int readNum = client.Receive(buffer, 0, buffer.Length, SocketFlags.None);
-                string readString = Encoding.Unicode.GetString(buffer, 0, readNum);
-                Console.WriteLine(DateTime.Now.ToString() + "  >>>  " + readString);
-                client.Send(Encoding.Unicode.GetBytes(DateTime.Now.ToString()));
-                client.Shutdown(SocketShutdown.Both);
-                client.Close();
-                switch (readString.ToLower())
+                using (var stream = client.GetStream())
                 {
-                    case "cls":
-                        Console.Clear();
-                        break;
-                    case "dir":
-                        Console.WriteLine(Directory.GetCurrentDirectory());
-                        break;
-                    case "time":
-                        Console.WriteLine(DateTime.Now);
-                        break;
+                    var buffer = Encoding.Unicode.GetBytes(line);
+                    stream.Write(buffer, 0, buffer.Length);
+                    buffer = new byte[1024];
+                    int count = stream.Read(buffer, 0, buffer.Length);
+                    Console.WriteLine(Encoding.Unicode.GetString(buffer,0,count));
                 }
-                if (readString == "exit")
-                {
-                    break;
-                }
+                tcpclient.Close();
             }
-            socket.Close();
         }
+
     }
-
 }
