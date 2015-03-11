17cecaea39630300decf854beba1365c0995fa62
luchunminglu <luchunminglu@gmail.com>
2015-03-11 15:54:50 +0800

    cc

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3Tests/OpenSourceLib/Utils/quartzs/QuartzHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Introduce_To_Algorithm3.OpenSourceLib.Utils;
+using Introduce_To_Algorithm3.OpenSourceLib.Utils.quartzs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Introduce_To_Algorithm3.OpenSourceLib.Utils.Tests
+{
+    [TestClass()]
+    public class QuartzHelperTests
+    {
+
+        [TestMethod()]
+        public void StartTest()
+        {
+            QuartzHelper quartzHelper = QuartzHelper.GetInstance();
+            quartzHelper.Start();
+            quartzHelper.ScheduleJob(quartzHelper.CreateJob("firstjob", typeof(JobImpl)), quartzHelper.CreateCronTrigger("firstTrigger", 5, "0/5 * * * * ?"));
+            Console.ReadLine();
+            quartzHelper.Shutdown();
+        }
+
+    }
+}
