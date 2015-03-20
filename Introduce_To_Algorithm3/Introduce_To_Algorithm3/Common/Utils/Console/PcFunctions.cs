df787644bbf09f080a26a6b49ab0b3ab83773921
luchunminglu <luchunminglu@gmail.com>
2015-03-20 16:44:01 +0800

    cc

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/Console/PcFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduce_To_Algorithm3.Common.Utils.Console
+{
+    public class PcFunctions
+    {
+        /// <summary>
+        /// 锁定工作台的显示，效果等同于开始按钮的锁定和CTRL+ALT+DEL
+        /// 该方法异步执行
+        /// </summary>
+        [DllImport("user32")]
+        public static extern void LockWorkStation();
+    }
+}
