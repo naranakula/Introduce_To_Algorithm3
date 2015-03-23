f2a0f5f3a2fb90270c2d9d892340b737315c9b96
luchunminglu <luchunminglu@gmail.com>
2015-03-23 17:04:25 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Program.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Program.cs
@@ -34,4 +34,41 @@ namespace Introduce_To_Algorithm3
         {
         }
     }
+
+    /*
+     * 
+    //正确使用wcf的示例
+    CalculatorClient wcfClient = new CalculatorClient();
+    try
+    {
+        Console.WriteLine(wcfClient.Add(4, 6));
+        wcfClient.Close();
+    }
+    catch (TimeoutException timeProblem)
+    {
+          Console.WriteLine("The service operation timed out. " + timeProblem.Message);
+          Console.ReadLine();
+          wcfClient.Abort();
+    }
+    catch (FaultException<GreetingFault> greetingFault)
+        {
+          Console.WriteLine(greetingFault.Detail.Message);
+          Console.ReadLine();
+          wcfClient.Abort();
+        }
+        catch (FaultException unknownFault)
+        {
+          Console.WriteLine("An unknown exception was received. " + unknownFault.Message);
+          Console.ReadLine();
+          wcfClient.Abort();
+        }
+        catch (CommunicationException commProblem)
+        {
+          Console.WriteLine("There was a communication problem. " + commProblem.Message + commProblem.StackTrace);
+          Console.ReadLine();
+          wcfClient.Abort();
+        }*/
+
+
+
 }
