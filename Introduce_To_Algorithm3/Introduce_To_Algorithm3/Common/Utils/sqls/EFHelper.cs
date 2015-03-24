35d96a081db879ad6861a38675e1d48679e076dc
luchunminglu <luchunminglu@gmail.com>
2015-03-24 09:41:08 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/sqls/EFHelper.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/sqls/EFHelper.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,15 @@ namespace Introduce_To_Algorithm3.Common.Utils.sqls
 {
     public static class EFHelper
     {
+        /// <summary>
+        /// 将高层的DbContext转换为较低阶的ObjectContext
+        /// </summary>
+        /// <param name="dbContext"></param>
+        /// <returns></returns>
+        public static ObjectContext ConvertTo(this DbContext dbContext)
+        {
+            IObjectContextAdapter ioca = dbContext as IObjectContextAdapter;
+            return ioca == null ? null : ioca.ObjectContext;
+        }
     }
 }
