22e532749876b6e9293ff148b96532921d465ffb
luchunminglu <luchunminglu@gmail.com>
2015-04-28 08:47:06 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/SuperSocket/Server/CustomServer.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/SuperSocket/Server/CustomServer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SuperSocket.Facility.Protocol;
 using SuperSocket.SocketBase;
 using SuperSocket.SocketBase.Protocol;
 
@@ -16,41 +17,43 @@ namespace Introduce_To_Algorithm3.OpenSourceLib
 
         #region 构造函数
 
-        //public CustomServer()
-        //    : base(new CommandLineReceiveFilterFactory())
-        //{
-        //    //
-        //    //默认的命令行协议要求每个请求必须以”\r\n”结束，空格分隔命令、各个参数。在SuperSocket中命令行协议会翻译成StringRequestInfo实例。
-        //}
+        public CustomServer()
+            : base(new CommandLineReceiveFilterFactory())
+        {
+            //
+            //默认的命令行协议要求每个请求必须以”\r\n”结束，空格分隔命令、各个参数。在SuperSocket中命令行协议会翻译成StringRequestInfo实例。
+        }
 
 
         /// <summary>
         /// 构造函数
         /// 使用定制的命令行协议
         /// </summary>
-        public CustomServer():base(new CommandLineReceiveFilterFactory(Encoding.UTF8,new BasicRequestInfoParser(":",",")))
+        public CustomServer(CommandLineReceiveFilterFactory filterFactory)
+            : base(new CommandLineReceiveFilterFactory(Encoding.UTF8, new BasicRequestInfoParser(":", ",")))
         {
             //定制的协议 请求的 key 和 body 通过字符 ':' 分隔, 而且多个参数被字符 ',' 分隔。
         }
 
-        ///// <summary>
-        ///// 终止符协议
-        ///// </summary>
-        //public CustomServer():base(new TerminatorReceiveFilterFactory("###"))
-        //{
-        //    //结束符协议
-        //    //一个协议使用三个字符 "###" 作为结束符
-        //}
-
         /// <summary>
-        /// 固定数量分隔符协议
+        /// 终止符协议
         /// </summary>
-        public CustomServer(int i)
-            : base()
+        public CustomServer(TerminatorReceiveFilterFactory filterFactory=null)
+            : base(new TerminatorReceiveFilterFactory("###"))
         {
             //结束符协议
             //一个协议使用三个字符 "###" 作为结束符
         }
+
+        /// <summary>
+        /// 固定数量分隔符协议
+        /// </summary>
+        public CustomServer(CountSpliterReceiveFilterFactory filterFactory)
+            : base(new CountSpliterReceiveFilterFactory((byte)'#',8))
+        {
+            //协议定义了像这样格式的请求 "#part1#part2#part3#part4#part5#part6#part7#". 每个请求有7个由 '#' 分隔的部分. 
+        }
+
 
         #endregion
 
