cabcc4fde86f88f384179fde6d2b468461a0f764
luchunminglu <luchunminglu@gmail.com>
2015-05-17 19:34:07 +0800

    cc

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/Des3Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduce_To_Algorithm3.Common.Utils
+{
+    /// <summary>
+    /// 关于三重DES加密有四种不同的模式，罗列如下：
+    ////1. DES-EEE3模式。使用三个不同密钥（k1,k2,k3），采用三次加密算法。
+    ////2. DES-EDE3模式。使用三个不同密钥（k1,k2,k3），采用加密-解密-加密算法。
+    ////3. DES-EEE2模式。使用两个不同密钥（k1 = k3,k2），采用三次加密算法。
+    ////4. DES-EDE2模式。使用两个不同密钥（k1 = k3,k2），采用加密-解密-加密算法。
+    /// </summary>
+    public static class Des3Helper
+    {
+    }
+}
