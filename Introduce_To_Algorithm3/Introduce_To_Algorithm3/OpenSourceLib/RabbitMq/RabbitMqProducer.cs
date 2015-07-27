8f74ab336265a96ead75668c6d5fcb2cfcceb55f
luchunminglu <luchunminglu@gmail.com>
2015-07-27 15:07:54 +0800

    cc

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/RabbitMq/RabbitMqProducer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Introduce_To_Algorithm3.OpenSourceLib.RabbitMq
+{
+    public static class RabbitMqProducer
+    {
+
+        public static void Send(string message)
+        {
+            var factory = new ConnectionFactory(){HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                }
+            }
+        }
+    }
+}
