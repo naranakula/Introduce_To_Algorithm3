5c4631089b1093b3f9482e2f07b34f98d96f4a2c
luchunminglu <luchunminglu@gmail.com>
2015-09-08 12:03:01 -0400

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Program.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Program.cs
@@ -24,148 +24,23 @@ using Quartz.Util;
 
 namespace Introduce_To_Algorithm3
 {
-    internal class Program
+    public class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Database.SetInitializer(new Initializer());
-            using (BaseContext context = new BaseContext())
-            {
-                Phone phone = context.People.Include(p=>p.Phones).First(r=>r.FirstName=="A").Phones.FirstOrDefault();
-                
-                Console.WriteLine(phone);
-            }
-            Thread.Sleep(1000);
-        
+            Console.WriteLine("Begin categorical data clustering demo");
+
+            string[][] rawData = new string[7][];
+            rawData[0] = new string[] { "Blue", "Small", "False" }; 
+            rawData[1] = new string[] { "Green", "Medium", "True" };
+            rawData[2] = new string[] { "Red", "Large", "False" };
+            rawData[3] = new string[] { "Red", "Small", "True" };
+            rawData[4] = new string[] { "Green", "Medium", "False" };
+            rawData[5] = new string[] { "Yellow", "Medium", "False" };
+            rawData[6] = new string[] { "Red", "Large", "False" };
+            Console.WriteLine("Raw unclustered data: ");
+            Console.WriteLine(" Color Size Heavy");
+            Console.WriteLine("-----------------------------");
         }
-    }
-
-    public class Initializer:CreateDatabaseIfNotExists<BaseContext>
-    {
-        public override void InitializeDatabase(BaseContext context)
-        {
-            base.InitializeDatabase(context);
-        }
-
-        protected override void Seed(BaseContext context)
-        {
-            //可以在数据库创建后运行的程序，如添加初始数据的代码
-            Person person = new Person() {FirstName = "A", LastName = "N", MidName = "L"};
-            person.Companies.Add(new Company(){CompanyName = "com"});
-            person.Phones.Add(new Phone(){PhoneNumber = "1522121"});
-            person.Student = new Student(){CollegeName = "zhejiang"};
-
-            context.People.Add(person);
-            
-            context.SaveChanges();
-        }
-    }
-
-    public class BaseContext : DbContext
-    {
-        public BaseContext() : base("name=ConnString")
-        {
-            
-        }
-
-        /// <summary>
-        /// 数据库表的名称不是由People生成的
-        /// </summary>
-        public DbSet<Person> People { get; set; }
-
-
-        public DbSet<Phone> Phone { get; set; } 
-
-        /// <summary>
-        /// 创建数据库的第一个实例时仅调用此方法一次
-        /// </summary>
-        /// <param name="modelBuilder"></param>
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            //modelBuilder.Entity<Person>()
-            //    .Property(p => p.FirstName)
-            //    .HasColumnName("FirstName")
-            //    .HasMaxLength(30)
-            //    .IsFixedLength()
-            //    .IsUnicode(true).IsRequired();
-
-            modelBuilder.Configurations.Add(new PersonMap());
-            modelBuilder.Configurations.Add(new StudentMap());
-        }
-    }
-
-    public class PersonMap : EntityTypeConfiguration<Person>
-    {
-        public PersonMap()
-        {
-            ToTable("Person");
-            Property(p => p.FirstName).HasMaxLength(30)/*.IsOptional().IsFixedLength()*/.IsVariableLength().IsUnicode(true).IsRequired();
-            HasMany(p => p.Phones).WithOptional(phone=>  phone.Person ).HasForeignKey(ph => ph.PersonId)/*.WillCascadeOnDelete(true)*/;
-            HasMany(p => p.Companies).WithMany(c => c.Persons).Map(m =>
-            {
-                m.MapLeftKey("PersonId");
-                m.MapRightKey("CompanyId");
-                m.ToTable("PersonCompany");
-            });
-
-        }
-    }
-
-    public class StudentMap : EntityTypeConfiguration<Student>
-    {
-        public StudentMap()
-        {
-            HasKey(s=>s.PersonId).HasRequired(s => s.Person).WithOptional(p => p.Student);
-        }
-    }
-
-    public class Student
-    {
-        public int PersonId { get; set; }
-        public virtual Person Person { get; set; }
-        public string CollegeName { get; set; }
-    }
-
-    public class Company
-    {
-        public int CompanyId { get; set; }
-        public string CompanyName { get; set; }
-        public virtual ICollection<Person> Persons { get; set; } 
-    }
-
-    public class Person
-    {
-        public Person()
-        {
-            Phones = new List<Phone>();
-            Companies = new List<Company>();
-        }
-
-        public int PersonId { get; set; }
-        public string FirstName { get; set; }
-        [MaxLength(30)]
-        [Column(TypeName = "nvarchar")]
-        public string LastName { get; set; }
-        public string MidName { get; set; }
-        public virtual Student Student { get; set; }
-
-        public virtual ICollection<Phone> Phones { get; set; }
-
-        public virtual ICollection<Company> Companies { get; set; } 
-
-    }
-
-    public class Phone
-    {
-        public Phone()
-        {
-        }
-
-        public int PhoneId { get; set; }
-        public string PhoneNumber { get; set; }
-        public int? PersonId { get; set; }
-
-        public virtual Person Person { get; set; }
-
     }
 }
