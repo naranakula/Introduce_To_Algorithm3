746b53622be6c67343fb31819474b372e7d9ac62
luchunminglu <luchunminglu@gmail.com>
2015-11-17 16:37:34 +0800

    cc

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/sockets/SocketEx.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduce_To_Algorithm3.Common.Utils.sockets
+{
+    /// <summary>
+    /// Socket拓展类
+    /// </summary>
+    public static class SocketEx
+    {
+        /// <summary>
+        /// Close the socket safely
+        /// </summary>
+        /// <param name="socket"></param>
+        public static void SafeClose(this Socket socket)
+        {
+            if (socket == null)
+            {
+                return;
+            }
+
+            try
+            {
+                socket.Shutdown(SocketShutdown.Both);
+            }
+            catch
+            {
+            }
+
+            try
+            {
+                socket.Close();
+            }
+            catch
+            {
+            }
+            finally
+            {
+                socket = null;
+            }
+        }
+    }
+}
