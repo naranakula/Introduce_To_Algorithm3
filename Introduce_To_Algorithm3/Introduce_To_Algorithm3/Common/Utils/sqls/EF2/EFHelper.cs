d1478da26005b032aad9bd9908e5e6569e8a5086
luchunminglu <luchunminglu@gmail.com>
2015-11-21 15:48:54 +0800

    cc

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/sqls/EF2/EFHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduce_To_Algorithm3.Common.Utils.sqls.EF2
+{
+    /// <summary>
+    /// Entity Framework 帮助类
+    /// </summary>
+    public static class EfHelper
+    {
+    }
+}
