2d3c1f4682aad1109ad0687bbd93578f25378574
luchunminglu <luchunminglu@gmail.com>
2016-01-04 12:32:24 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/Json/JsonHelper.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/Json/JsonHelper.cs
@@ -31,6 +31,7 @@ namespace Introduce_To_Algorithm3.OpenSourceLib.Json
             _jsonSerializerSettings = new JsonSerializerSettings()
             {
                 MissingMemberHandling = MissingMemberHandling.Ignore,//当反序列化时，如果json字符串中包含不在对象中的成员，则忽略而不抛异常,这是Json默认的处理方式
+                //经试验证明，当json缺少包含在对象中的成员时，也可以反序列化
                 ReferenceLoopHandling = ReferenceLoopHandling.Ignore,//序列化时忽略循环引用， The first time an object is encountered it will be serialized as usual but if the object is encountered as a child object of itself the serializer will skip serializing it.，默认的方式是Error
                 NullValueHandling = NullValueHandling.Include,//Json.NET writes null values to JSON when serializing and sets null values to fields/properties when deserializing.这是json默认的处理方式
                 DefaultValueHandling = DefaultValueHandling.Include,//这是默认的处理方式，Json.NET will write a field/property value to JSON when serializing if the value is the same as the field/property's default value. The Json.NET deserializer will continue setting a field/property if the JSON value is the same as the default value.
