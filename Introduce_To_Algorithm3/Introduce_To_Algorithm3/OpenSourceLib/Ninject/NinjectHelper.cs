469abd8849121ed7a5aa69e41e8a9aec4c743fcf
luchunminglu <luchunminglu@gmail.com>
2016-03-05 19:56:13 +0800

    cc

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/Ninject/NinjectHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduce_To_Algorithm3.OpenSourceLib.Ninject
+{
+    /// <summary>
+    /// Ninject帮助类，根据GitHub和Nuget上的下载量和Stars，Ninject比Autofac要好
+    /// 
+    /// Ninject是轻量级或者超轻量级的Dependency injector.
+    /// </summary>
+    public class NinjectHelper
+    {
+    }
+}
