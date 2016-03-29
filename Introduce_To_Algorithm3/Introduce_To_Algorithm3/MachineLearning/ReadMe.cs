da1d300a304d898542ce7d816edf4439de3b0ade
luchunminglu <luchunminglu@gmail.com>
2016-03-29 16:59:22 +0800

    cc

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/MachineLearning/ReadMe.cs
@@ -0,0 +1,15 @@
+﻿
+/*
+ * 常用的机器学习库：
+ * Mahout: Machine learning Java libraries.
+ * R:我不用
+ * Julia:我不用
+ * Python:NumPy  SciPy scikit-learn  次要： pydoop  LibSVM LibLinear OpenCv NLTK mlpy  pyml  pybrain  mdp-toolKit
+ * Spark:提供java接口
+ * Spring XD:支持java
+ * 
+ * 
+
+
+
+*/
