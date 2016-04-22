ef8a017c696feb7953b21cd3c74972d050350f54
luchunminglu <luchunminglu@gmail.com>
2016-04-22 20:06:26 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/MachineLearning/ClusterCategorical.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/MachineLearning/ClusterCategorical.cs
@@ -7,28 +7,512 @@ using System.Threading.Tasks;
 namespace Introduce_To_Algorithm3.Common.MachineLearning
 {
     /// <summary>
+    /// 详细见书：machine learning using C# succinctly
     /// 聚簇分类
     /// 具体思想是：每个分组先分配一个种子数据，对于剩下的数据项，确定分配到哪一个分组产生最大的CU,然后分配到该分组。
-    /// 好的方法是选择尽可能互不相同的 m 个元组作为种子元组。 这里就又该是分类效用发挥作用了，可以计算所有可能的候选种子元组集的 CU，使用具有最佳 CU（最大值表示最不相同）的元组作为种子元组。 如上所述，一般不可能检查每个可能的种子元组集，因此我们重复选择 m 个随机元组，计算这些随机元组的 CU，并使用具有最佳 CU 的元组集作为种子。
+    /// 好的方法是选择尽可能互不相同的 m 个元组作为种子元组。 这里就又该是分类效用发挥作用了，可以计算所有可能的候选种子元组集的 CU，使用具有最佳 CU（最大值表示最不相同）的元组作为种子元组。 如上所述，一般不可能检查每个可能的种子元组集，因此我们重复选择 m 个随机元组，计算这些随机元组应用算法产生的 CU，并使用具有最佳 CU 的分组作为最后分组。
+    /// 最后的分组是有初始的种子和检查的顺序决定
 
     /// </summary>
     public class ClusterCategorical
     {
         /// <summary>
+        /// 分组的数目
+        /// </summary>
+        private int numClusters;
+
+        /// <summary>
+        /// 分组 clusterings[i]=j 表示第i个元素的分组是j组
+        /// </summary>
+        private int[] clusterings;
+
+        /// <summary>
+        /// dataAsInts[i]表示第i个元素  dataAsInts[i][j]=m 表示第i个元素的第j个属性的属性值是m
+        /// </summary>
+        private int[][] dataAsInts;
+
+        /// <summary>
+        /// 原生数据
+        /// </summary>
+        private string[][] rawDatas;
+
+        /// <summary>
+        /// valueCounts[i][j][k]=m i表示属性的分类  j表示属性的值  k表示分组  m表示该分组中的属性的值的个数
+        /// </summary>
+        private int[][][] valueCounts;
+
+        /// <summary>
+        /// clusterCounts[i]=j  表示i个分组的元素的个数是j
+        /// </summary>
+        private int[] clusterCounts;
+
+        /// <summary>
+        /// 随机数
+        /// </summary>
+        private Random rnd;
+
+
+        /// <summary>
         /// 构造函数
         /// </summary>
-        /// <param name="numClusters"></param>
-        /// <param name="rawData"></param>
+        /// <param name="numClusters">分组数目</param>
+        /// <param name="rawData">rowData表示要分组的原生数据</param>
         public ClusterCategorical(int numClusters, string[][] rawData)
         {
+            this.numClusters = numClusters;
+            this.rawDatas = rawData;
+            if (numClusters<=1)
+            {
+                throw  new Exception("分组数不能小于1");
+            }
+
+            if (rawData == null || numClusters > rawData.Length)
+            {
+                throw new Exception("分组大于要元素数目");
+            }
             
+            //表示转换
+            MakeDataMatrix(rawData);
+            //分配数据结构
+            Allocate();
         }
 
-        private int[] Cluster(int numRestarts, out double cu)
+        /// <summary>
+        /// 执行算法的主要部分
+        /// </summary>
+        /// <param name="numRestarts">重试次数</param>
+        /// <param name="cu"></param>
+        /// <returns></returns>
+        public int[] Cluster(int numRestarts, out double cu)
         {
-            throw new NotImplementedException();
+            int numRows = dataAsInts.Length;
+            double currCU=0, bestCU = -1;
+            int[] bestClustering = new int[numRows];
+            for (int i = 0; i < numRestarts; i++)
+            {
+                //为了保持每次运行的结果一致，实际情况下应该使用更随机的，如当前时间
+                //int seed = i + i*i;
+                int seed = i;//use i as rnd seed
+                int[] currClustrings = ClusterOnce(seed, out currCU);
+                if (currCU > bestCU)
+                {
+                    bestCU = currCU;
+                    Array.Copy(currClustrings,bestClustering,numRows);
+                }
+            }
+            cu = bestCU;
+            return bestClustering;
         }
 
+        /// <summary>
+        /// 执行算法一次
+        /// </summary>
+        /// <param name="seed">随机数的种子</param>
+        /// <param name="catUtility"></param>
+        /// <returns></returns>
+        private int[] ClusterOnce(int seed, out double catUtility)
+        {
+            this.rnd = new Random(seed);
+            //this.rnd = new Random();//更好的方法是使用时间相关的默认种子
+
+            //初始化
+            Initialize();
+
+            //选择初始的分组种子
+            int numTrials = dataAsInts.Length;
+            int[] goodIndexes = GetGoodIndices(numTrials);
+            for (int i = 0; i < numClusters; i++)
+            {
+                //将初始的分组种子赋给每个分组一个
+                Assign(goodIndexes[i],i);
+            }
+
+            int numRows = dataAsInts.Length;
+            //随机重排待分组元素
+            int[] rndSequence = new int[numRows];
+            for (int i = 0; i < numRows; i++)
+            {
+                rndSequence[i] = i;
+            }
+
+            
+            Shuffle(rndSequence);
+
+            //算法的核心：
+            /*
+             * 接下来检测每个未被份组的元素，将其放到各个分组
+             * 判断每次产生的cu，最大的cu的分组选为最终的分组
+             * 
+             */
+            for (int i = 0; i < numRows; i++)
+            {
+                int idx = rndSequence[i];//选择要处理的元素
+                if (clusterings[idx] >= 0)
+                {
+                    //已经分组的元素
+                    continue;
+                }
+
+                double[] candidateCU = new double[numClusters];
+                for (int j = 0; j < numClusters; j++)
+                {
+                    //判断每次放置，新分组产生的cu
+                    Assign(idx,j);
+                    candidateCU[j] = CategoryUtility();//计算cu
+                    Unassign(idx,j);//undo assign
+                }
+
+                //获取最好的cu的分组
+                int bestCluster = MaxIndex(candidateCU);
+                Assign(idx,bestCluster);
+            }
+
+            //获取最后的cu
+            catUtility = CategoryUtility();
+            int[] result = new int[numRows];
+            Array.Copy(this.clusterings,result,numRows);
+            return result;
+        }
+
+        /// <summary>
+        /// 将字符串表示的原生数据，转换为整数表示
+        /// </summary>
+        /// <param name="rawData"></param>
+        /// <returns></returns>
+        private int[][] MakeDataMatrix(string[][] rawData)
+        {
+            int numRows = rawData.Length;
+            int numCols = rawData[0].Length;
+
+            this.dataAsInts = new int[numRows][];
+
+            for (int i = 0; i < numRows; i++)
+            {
+                dataAsInts[i] = new int[numCols];
+            }
+
+            for (int col = 0; col < numCols; col++)
+            {
+                int idx = 0;
+                Dictionary<String,int> dict = new Dictionary<string, int>();
+
+                for (int row = 0; row < numRows; row++)
+                {
+                    string s = rawData[row][col].Trim().ToLower();
+                    //计算每个属性值的int，每个属性类别从0开始
+                    if (!dict.ContainsKey(s))
+                    {
+                        dict.Add(s,idx);
+                        idx++;
+                    }
+                }
+
+                for (int row = 0; row < numRows; row++)
+                {
+                    string s = rawData[row][col].Trim().ToLower();
+                    int v = dict[s];
+                    this.dataAsInts[row][col] = v;
+                }
+
+            }
+
+            return dataAsInts;
+        }
+
+        /// <summary>
+        /// 为本算法使用的数据结构分配内存空间
+        /// </summary>
+        private void Allocate()
+        {
+            int numRows = dataAsInts.Length;
+            int numCols = dataAsInts[0].Length;
+
+            this.clusterings = new int[numRows];
+            //最后一个存放所有的元素的数目
+            this.clusterCounts = new int[numClusters+1];
+
+            this.valueCounts = new int[numCols][][];
+            for (int col = 0; col < numCols; col++)
+            {
+                int maxVal = 0;
+                //获取列数-1
+                for (int i = 0; i < numRows; i++)
+                {
+                    if (dataAsInts[i][col]>maxVal)
+                    {
+                        maxVal = dataAsInts[i][col];
+                    }
+                }
+
+                this.valueCounts[col] = new int[maxVal+1][];
+            }
+
+            for (int i = 0; i < valueCounts.Length; i++)
+            {
+                for (int j = 0; j < valueCounts[i].Length; j++)
+                {
+                    //最后一个存放所有分组中的的属性分类i的属性值j的和
+                    this.valueCounts[i][j] = new int[numClusters+1];
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        private void Initialize()
+        {
+            for (int i = 0; i < clusterings.Length; i++)
+            {
+                clusterings[i] = -1;
+            }
+
+            for (int i = 0; i < clusterCounts.Length; i++)
+            {
+                clusterCounts[i] = 0;
+            }
+
+            for (int i = 0; i < valueCounts.Length; i++)
+            {
+                for (int j = 0; j < valueCounts[i].Length; j++)
+                {
+                    for (int k = 0; k < valueCounts[i][j].Length; k++)
+                    {
+                        valueCounts[i][j][k] = 0;
+                    }
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// 计算cu
+        /// 最难的部分
+        /// </summary>
+        /// <returns></returns>
+        private double CategoryUtility()
+        {
+            //获取共有多少个元素
+            int numTuplesAssigned = clusterCounts[clusterCounts.Length - 1];
+            //每个分组的概率
+            double[] clusterProbs = new double[this.numClusters];
+
+            for (int i = 0; i < numClusters; i++)
+            {
+                clusterProbs[i] = clusterCounts[i]*1.0/numTuplesAssigned;
+            }
+
+            //计算无条件概率，方程式右边项
+            double unconditional = 0.0;//保存无条件概率
+            for (int i = 0; i < valueCounts.Length; i++)
+            {
+                for (int j = 0; j < valueCounts[i].Length; j++)
+                {
+                    //last cell holds sum
+                    int sum = valueCounts[i][j][numClusters];
+                    double p = (sum*1.0)/numTuplesAssigned;
+                    unconditional += p*p;
+                }
+            }
+
+            //numCluster个条件概率
+            double[] conditionals = new double[numClusters];
+            for (int k = 0; k < numClusters; k++)
+            {
+                //每个属性种类
+                for (int i = 0; i < valueCounts.Length; i++)
+                {
+                    //每个属性值
+                    for (int j = 0; j < valueCounts[i].Length; j++)
+                    {
+                        //条件概率
+                        double p = (valueCounts[i][j][k]*1.0)/clusterCounts[k];
+                        conditionals[k] += (p*p);
+                    }
+                }
+            }
+
+            //大括号的差
+            double summation = 0.0;
+            for (int i = 0; i < numClusters; i++)
+            {
+                summation += clusterProbs[i]*(conditionals[i]-unconditional);
+            }
+
+            return summation/numClusters;
+        }
+
+        /// <summary>
+        /// 返回最好的cu所在的分组索引
+        /// </summary>
+        /// <param name="cus"></param>
+        /// <returns></returns>
+        private static int MaxIndex(double[] cus)
+        {
+            double bestCU = 0.0;
+            int indexOfBestCU = 0;
+            for (int i = 0; i < cus.Length; i++)
+            {
+                if (cus[i]>bestCU)
+                {
+                    indexOfBestCU = i;
+                    bestCU = cus[i];
+                }
+            }
+
+            return indexOfBestCU;
+        }
+
+        /// <summary>
+        /// 重排元素
+        /// </summary>
+        /// <param name="indices"></param>
+        private void Shuffle(int[] indices)
+        {
+            for (int i = 0; i < indices.Length; i++)
+            {
+                int ri = rnd.Next(i, indices.Length);
+                int tmp = indices[i];
+                indices[i] = indices[ri];
+                indices[ri] = tmp;
+            }
+        }
+
+        /// <summary>
+        /// 将元素dataIndex放到分组clusterId
+        /// </summary>
+        /// <param name="dataIndex"></param>
+        /// <param name="clusterId"></param>
+        private void Assign(int dataIndex, int clusterId)
+        {
+            clusterings[dataIndex] = clusterId;
+
+            //更新valueCounts clusterCounts
+            for (int i = 0; i < valueCounts.Length; i++)
+            {
+                int v = dataAsInts[dataIndex][i];
+                ++valueCounts[i][v][clusterId];
+                ++valueCounts[i][v][numClusters];
+            }
+
+            ++clusterCounts[clusterId];
+            ++clusterCounts[numClusters];
+        }
+
+        /// <summary>
+        /// 将元素dataIndex从分组clusterId中剔除
+        /// </summary>
+        /// <param name="dataIndex"></param>
+        /// <param name="clusterId"></param>
+        private void Unassign(int dataIndex, int clusterId)
+        {
+            clusterings[dataIndex] = -1;
+            //更新valueCounts clusterCounts
+            for (int i = 0; i < valueCounts.Length; i++)
+            {
+                int v = dataAsInts[dataIndex][i];
+                --valueCounts[i][v][clusterId];
+                --valueCounts[i][v][numClusters];
+            }
+
+            --clusterCounts[clusterId];
+            --clusterCounts[numClusters];
+
+        }
+
+        /// <summary>
+        /// 获取好的初始种子
+        /// 首先选出来的种子必须不同
+        /// 第二种子之间的差异越大越好
+        /// </summary>
+        /// <param name="numTrials">重试次数</param>
+        /// <returns></returns>
+        private int[] GetGoodIndices(int numTrials)
+        {
+            int numRows = dataAsInts.Length;
+            int numCols = dataAsInts[0].Length;
+
+            int[] result = new int[numClusters];
+
+            //找到的初始分组的原则是使他们尽可能不同
+            int largestDiff = -1;
+            for (int trial = 0; trial < numTrials; trial++)
+            {
+                int[] candidates = Reservoir(numClusters, numRows);
+                int numDifferences = 0;//不同属性的个数
+
+                for (int i = 0; i < candidates.Length-1; i++)
+                {
+                    for (int j = i+1; j < candidates.Length; j++)
+                    {
+                        //比较属性值不同的个数
+                        int aRow = candidates[i];
+                        int bRow = candidates[j];
+
+                        for (int col = 0; col < numCols; col++)
+                        {
+                            if (dataAsInts[aRow][col] != dataAsInts[bRow][col])
+                            {
+                                //属性值不同
+                                numDifferences++;
+                            }
+                        }
+
+                    }
+                }
+
+                if (numDifferences>largestDiff)
+                {
+                    largestDiff = numDifferences;
+                    Array.Copy(candidates,result,numClusters);
+                }
+
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 查找n个不同的样本，GetGoodIndices的辅助方法
+        /// 从range中选取n个不同的样本
+        /// 蓄水池抽样，从range个抽取n个不同的样本，每个的抽到的概率相同
+        /// 
+        /// 具体的算法解释：
+        /// 该方法类似于从数据流中选取n个数据，因为我们不知道最终数据流中有多少数据，我们选取前n个，对于每下一个我们知道它被选取的概率是n/目前观察到的数量，如果随机数小于这个数，我们选取他，并在现有的数中随机挑一个替换
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        private int[] Reservoir(int n, int range)
+        {
+            //保存最后的结果
+            int[] result = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = i;
+            }
+
+            for (int i = n; i < range; i++)
+            {
+                int j = rnd.Next(0, i + 1);//包含下界，不包含上界
+                if (j<n)
+                {
+                    result[j] = i;
+                }
+            }
+
+            return result;
+        }
+
+
+
+        #region 演示程序，不具备通用性
+
+        /// <summary>
+        /// 测试程序
+        /// </summary>
         public static void TestMain()
         {
             Console.WriteLine("Begin categorical data clustering demo");
@@ -46,9 +530,10 @@ namespace Introduce_To_Algorithm3.Common.MachineLearning
             Console.WriteLine("-----------------------------");
 
             ShowData(rawData);
-
+            //分组的个数
             int numClusters = 2;
             Console.WriteLine("Setting numClusters to "+numClusters);
+            //重试的次数
             int numRestarts = 4;
             Console.WriteLine("Setting numRestarts to "+numRestarts);
 
@@ -67,20 +552,74 @@ namespace Introduce_To_Algorithm3.Common.MachineLearning
             Console.WriteLine("end categorical data");
         }
 
+
+        /// <summary>
+        /// 显示最终结果
+        /// </summary>
+        /// <param name="numClusters"></param>
+        /// <param name="clustering"></param>
+        /// <param name="rawData"></param>
         private static void ShowClustering(int numClusters, int[] clustering, string[][] rawData)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("-----------------------------------------------");
+            for (int k = 0; k < numClusters; k++)
+            {
+                for (int i = 0; i < rawData.Length; i++)
+                {
+                    if (clustering[i] == k)
+                    {
+                        //是第k个分组的，打印该元素
+                        Console.Write(i.ToString().PadLeft(2)+"  ");
+
+                        for (int j = 0; j < rawData[i].Length; j++)
+                        {
+                            Console.Write(rawData[i][j].ToString().PadRight(8)+"  ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+
+                Console.WriteLine("---------------------------------------");
+            }
         }
 
-        private static void ShowVector(int[] clustering, bool p)
+        /// <summary>
+        /// 显示分组
+        /// </summary>
+        /// <param name="clustering"></param>
+        /// <param name="newLine"></param>
+        private static void ShowVector(int[] clustering, bool newLine)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < clustering.Length; i++)
+            {
+                Console.Write(clustering[i]+"  ");
+            }
+            Console.WriteLine();
+            if (newLine)
+            {
+                Console.WriteLine();
+            }
         }
 
         
-
+        /// <summary>
+        /// 显示源数据
+        /// </summary>
+        /// <param name="rawData"></param>
         private static void ShowData(string[][] rawData)
         {
+            for (int i = 0; i < rawData.Length; i++)
+            {
+                Console.Write("["+i+"]");
+                for (int j = 0; j < rawData[i].Length; j++)
+                {
+                    Console.Write(rawData[i][j].ToString().PadRight(8)+"  ");
+                }
+                Console.WriteLine();
+            }
         }
+
+        #endregion
+
     }
 }
---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/MachineLearning/LogisticClassifier.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduce_To_Algorithm3.Common.MachineLearning
+{
+    /// <summary>
+    /// LogisticRegressionWithSimplexOptimization
+    /// 
+    /// 详见：Machine Learning Using C# Succinctly
+    /// </summary>
+    public class LogisticClassifier
+    {
+        private int numFeatures;
+
+        public LogisticClassifier(int numFeatures)
+        {
+            // TODO: Complete member initialization
+            this.numFeatures = numFeatures;
+        }
+
+
+        /// <summary>
+        /// 逻辑回归的主要算法
+        /// </summary>
+        /// <param name="trainData">训练数据</param>
+        /// <param name="maxtry">最大应用算法的重试次数</param>
+        /// <param name="seed">随机数种子</param>
+        /// <returns></returns>
+        private double[] Train(double[][] trainData, int maxtry, int seed)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private double Accuracy(double[][] trainData, double[] bestWeights)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        #region 测试程序
+
+        public static void TestMain()
+        {
+            Console.WriteLine("Begin Logistic Regression Binary Classification demo");
+            double[][] data = new double[30][];
+            data[0] = new double[] { 48, +1, 4.40, 0 }; 
+            data[1] = new double[] { 60, -1, 7.89, 1 };
+            data[2] = new double[] { 51, -1, 3.48, 0 }; 
+            data[3] = new double[] { 66, -1, 8.41, 1 }; 
+            data[4] = new double[] { 40, +1, 3.05, 0 }; 
+            data[5] = new double[] { 44, +1, 4.56, 0 }; 
+            data[6] = new double[] { 80, -1, 6.91, 1 }; 
+            data[7] = new double[] { 52, -1, 5.69, 0 }; 
+            data[8] = new double[] { 56, -1, 4.01, 0 }; 
+            data[9] = new double[] { 55, -1, 4.48, 0 }; 
+            data[10] = new double[] { 72, +1, 5.97, 0 }; 
+            data[11] = new double[] { 57, -1, 6.71, 1 }; 
+            data[12] = new double[] { 50, -1, 6.40, 0 }; 
+            data[13] = new double[] { 80, -1, 6.67, 1 }; 
+            data[14] = new double[] { 69, +1, 5.79, 0 }; 
+            data[15] = new double[] { 39, -1, 5.42, 0 }; 
+            data[16] = new double[] { 68, -1, 7.61, 1 }; 
+            data[17] = new double[] { 47, +1, 3.24, 0 }; 
+            data[18] = new double[] { 45, +1, 4.29, 0 }; 
+            data[19] = new double[] { 79, +1, 7.44, 1 }; 
+            data[20] = new double[] { 44, -1, 2.55, 0 }; 
+            data[21] = new double[] { 52, +1, 3.71, 0 };
+            data[22] = new double[] { 80, +1, 7.56, 1 }; 
+            data[23] = new double[] { 76, -1, 7.80, 1 };
+            data[24] = new double[] { 51, -1, 5.94, 0 };
+            data[25] = new double[] { 46, +1, 5.52, 0 }; 
+            data[26] = new double[] { 48, -1, 3.25, 0 };
+            data[27] = new double[] { 58, +1, 4.71, 0 };
+            data[28] = new double[] { 44, +1, 2.52, 0 };
+            data[29] = new double[] { 68, -1, 8.38, 1 };
+
+            Console.WriteLine("Raw data:");
+            Console.WriteLine(" Age Sex Kidney Died"); 
+            Console.WriteLine("=======================================");
+            ShowData(data, 5, 2, true);
+
+            Console.WriteLine("Normalizing age and kidney data");
+            int[] columns = new int[]{0,2};
+            double[][] means = Normalize(data, columns);
+
+            Console.WriteLine("Normalized  data: ");
+            ShowData(data,5,2,true);
+
+            Console.WriteLine("Creating train (80%) and test (20%) matrices");
+
+            double[][] trainData;
+            double[][] testData;
+            double trainPercent = 0.8;//训练数据比例
+
+            MakeTrainTest(data, 0, out trainData,out testData,trainPercent);
+
+            Console.WriteLine("Normalized training data: ");
+            ShowData(trainData,3,2,true);
+
+            int numFeatures = 3;
+            Console.WriteLine("Creating LR binary classifier");
+
+            LogisticClassifier lc = new LogisticClassifier(numFeatures);
+            int maxEpochs = 100;//最大循环次数，应用回归算法的最大次数
+            //33是随机数的种子
+            double[] bestWeights = lc.Train(trainData, maxEpochs, 33);
+
+            Console.WriteLine("Best weights found:");
+            ShowVector(bestWeights, 4, true);
+
+            double trainAccuracy = lc.Accuracy(trainData, bestWeights);
+            Console.WriteLine("Prediction accuracy on training data = "+trainAccuracy.ToString("F4"));
+            double testAccuracy = lc.Accuracy(testData, bestWeights);
+            Console.WriteLine("Prediction accuracy on test data = "+testAccuracy.ToString("F4"));
+
+            Console.WriteLine("End LR binary classification demo");
+
+        }
+
+        /// <summary>
+        /// 将数据分为trainPercent比例训练数据，1-trainPercent  测试数据
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="p1"></param>
+        /// <param name="trainData"></param>
+        /// <param name="testData"></param>
+        /// <param name="trainPercent"></param>
+        private static void MakeTrainTest(double[][] data, int p1, out double[][] trainData, out double[][] testData, double trainPercent)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 打印一维向量
+        /// </summary>
+        /// <param name="bestWeights"></param>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        private static void ShowVector(double[] bestWeights, int p1, bool p2)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 创建分组， 80%用于训练，20%用于测试
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="p"></param>
+        /// <param name="trainData"></param>
+        /// <param name="testData"></param>
+        private static void MakeTrainTest(double[][] data, int p, out double[][] trainData, out double[][] testData)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 归一化数据
+        /// 归一化后数值应该在[-5,5]之间，超出这个范围都偏离均值太多，应该检查
+        /// 理论上，不需要归一化，但归一化通常提供更好的正确率
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="columns"></param>
+        /// <returns>返回值保存每个属性项的平均值和标准差,第一行是平均值，第二行是标准差</returns>
+        private static double[][] Normalize(double[][] data, int[] columns)
+        {
+            int numRows = data.Length;
+            int numCols = data[0].Length;
+            //result保存每个属性项的平均值和标准差
+            //第一行是平均值，第二行是标准差
+            double[][] result = new double[2][];
+            for (int i = 0; i < 2; i++)
+            {
+                result[i] = new double[numCols];
+            }
+
+            //可以只计算columns指定的列
+            for (int c = 0; c < numCols; c++)
+            {
+                double sum = 0.0;
+                for (int r = 0; r < numRows; r++)
+                {
+                    sum += data[r][c];
+                }
+                double mean = sum/numRows;
+                result[0][c] = mean;
+
+                //计算标准差
+                double sumSquares = 0;
+                for (int r = 0; r < numRows; r++)
+                {
+                    sumSquares += (data[r][c] - mean)*(data[r][c] - mean);
+                }
+
+                double stdDev = System.Math.Sqrt(sumSquares/numRows);
+                result[1][c] = stdDev;
+            }
+
+            //高斯归一化
+            for (int c = 0; c < columns.Length; c++)
+            {
+                int j = columns[c];//需要归一化的列
+                double mean = result[0][c];
+                double stdDev = result[1][c];
+
+                for (int i = 0; i < numRows; i++)
+                {
+                    data[i][j] = (data[i][j] - mean)/stdDev;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 打印原生数据
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <param name="p3"></param>
+        private static void ShowData(double[][] data, int p1, int p2, bool p3)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
