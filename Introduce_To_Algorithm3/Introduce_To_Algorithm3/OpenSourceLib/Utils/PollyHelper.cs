aa816471b9cdae18c59fa613e812257632904563
luchunminglu <luchunminglu@gmail.com>
2016-06-14 15:58:11 +0800

    cc

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/Utils/PollyHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduce_To_Algorithm3.OpenSourceLib.Utils
+{
+    /// <summary>
+    /// polly allows developers to express transient exception handling policies such as Retry, Retry Forever, Wait and Retry or Circuit Breaker in a fluent manner.
+    /// </summary>
+    public static class PollyHelper
+    {
+    }
+}
