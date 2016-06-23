1e1ae0669b90fb0816269f7c0f0595fcc7e4db05
luchunminglu <luchunminglu@gmail.com>
2016-06-23 17:49:04 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Program.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Program.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Numerics;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
@@ -29,15 +30,66 @@ namespace Introduce_To_Algorithm3
 {
     public class Program
     {
+        [DllImport("user32.dll", EntryPoint = "ShowWindow", CharSet = CharSet.Auto)]
+        public static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+
+        [DllImport("User32.dll", EntryPoint = "FindWindow")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("User32.dll", EntryPoint = "FindWindowEx")]
+        private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpClassName, string lpWindowName);
+        /// <summary>
+        /// 最小化窗口，即使拥有窗口的线程被挂起也会最小化。在从其他线程最小化窗口时才使用这个参数。
+        /// </summary>
+        public const int SW_FORCEMINIMIZE = 11;
+
+        /// <summary>
+        /// 隐藏窗口并激活其他窗口。
+        /// </summary>
+        public const int SW_HIDE = 0;
+        /// <summary>
+        /// 窗口原来的位置以原来的尺寸激活和显示窗口。
+        /// </summary>
+        public const int SW_SHOW = 5;
+        /// <summary>
+        /// 最大化指定的窗口
+        /// </summary>
+        public const int SW_MAXIMIZE = 3;
+        /// <summary>
+        /// 最小化指定的窗口并且激活在Z序中的下一个顶层窗口。
+        /// </summary>
+        public const int SW_MINIMIZE = 6;
+
+        /// <summary>
+        /// 激活窗口并将其最小化
+        /// </summary>
+        public const int SW_SHOWMINIMIZED = 2;
+        /// <summary>
+        /// 激活窗口并将其最大化。
+        /// </summary>
+        public const int SW_SHOWMAXIMIZED = 3;
+        /// <summary>
+        /// 激活窗口并将其最小化。
+        /// </summary>
+        public const int SW_SHOWNOACTIVATE = 4;
+
         public static void Main(string[] args)
         {
-            
-            NLogHelper.Trace("Trace");
-            NLogHelper.Debug("Debug");
-            NLogHelper.Info("Info");
-            NLogHelper.Warn("Warn");
-            NLogHelper.Error("Error");
-            NLogHelper.Fatal("Fatal");
+            Process[] processArr = Process.GetProcessesByName("unityapp");
+            if (processArr.Length < 1)
+            {
+                return;
+            }
+
+            var intptr = FindWindow(null, "Unity3D");
+            Console.WriteLine(intptr);
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(2000);
+                ShowWindow(intptr, SW_MAXIMIZE);
+                Thread.Sleep(2000);
+                ShowWindow(intptr, SW_MINIMIZE);
+            }
+
 //            string sqlConStr = "Data Source=192.168.163.218;Initial Catalog=FidsContext0317;User ID=sa;Password=system2000,.";
 //            string mySqlConStr = "server=192.168.163.225;port=3306;database=qdcargo;uid=root;password=123456";
 //            MySqlHelper mySqlHelper = MySqlHelper.GetInstance(mySqlConStr);
