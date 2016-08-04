19e47a6ff205fc96796016802a0c6224c7c85c64
luchunminglu <luchunminglu@gmail.com>
2016-08-04 17:18:33 +0800

    cc

---
+++ Introduce_To_Algorithm3/OpenCVConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace OpenCVConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VideoCapture videoCapture = new VideoCapture(@"D:\YoukuFiles\download\1.mp4");
+            Console.WriteLine(videoCapture.Fps+""+videoCapture.IsOpened());
+            int sleepTime = (int)Math.Round(1000/25.0);
+            using (Window window = new Window("capture"))
+            {
+                using (Mat image = new Mat())
+                {
+                    while (true)
+                    {
+                        videoCapture.Read(image);
+                        if (image.Empty())
+                        {
+                            break;
+                        }
+                        window.ShowImage(image);
+                        Cv2.WaitKey(sleepTime);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Introduce_To_Algorithm3/OpenCVConsole/Utils/ImageUtils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace OpenCVConsole.Utils
+{
+    /// <summary>
+    /// 图像帮助类
+    /// </summary>
+    public static class ImageUtils
+    {
+        /// <summary>
+        /// 获取图片
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="imreadModes"></param>
+        /// <returns></returns>
+        public static Mat Read(string fileName,ImreadModes imreadModes)
+        {
+            return  new Mat(fileName,imreadModes);
+        }
+
+        /// <summary>
+        /// 写图片
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="mat"></param>
+        public static void Write(string fileName, Mat mat)
+        {
+            mat.ImWrite(fileName);
+        }
+
+        /// <summary>
+        /// 转换为bitmap
+        /// </summary>
+        /// <param name="mat"></param>
+        /// <returns></returns>
+        public static Bitmap ConvertToBitmap(Mat mat)
+        {
+            return mat.ToBitmap();
+        }
+    }
+}
