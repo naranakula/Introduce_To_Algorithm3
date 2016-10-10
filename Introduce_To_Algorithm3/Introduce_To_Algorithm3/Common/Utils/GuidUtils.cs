349b35d33fd9f4c18dcb9bdb585ff0dd411f346e
luchunminglu <luchunminglu@gmail.com>
2016-10-10 15:12:30 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/GuidUtils.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/GuidUtils.cs
@@ -5,10 +5,13 @@ using System.Text;
 
 namespace Introduce_To_Algorithm3.Common.Utils
 {
+    /// <summary>
+    /// GUID有16个字节
+    /// </summary>
     public static class GuidUtils
     {
         /// <summary>
-        /// the format is:
+        /// the format is: 字母小写 
         /// 8-4-4-12
         /// xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
         /// </summary>
@@ -16,6 +19,16 @@ namespace Introduce_To_Algorithm3.Common.Utils
         public static string GetGuid()
         {
             return Guid.NewGuid().ToString();
+        }
+
+        /// <summary>
+        /// 32位小写格式 
+        /// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        /// </summary>
+        /// <returns></returns>
+        public static string GetGuid2()
+        {
+            return Guid.NewGuid().ToString("N");
         }
     }
 }
