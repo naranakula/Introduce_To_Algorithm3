0acdb8995e0e1c9c9655f01183646205a35d5989
luchunminglu <luchunminglu@gmail.com>
2016-11-03 09:29:26 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Program.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Program.cs
@@ -12,26 +12,6 @@ namespace Introduce_To_Algorithm3
         public static void Main(string[] args)
         {
 
-
-
-
-            NetworkCommsClient.Init();
-            while (true)
-            {
-                string Input = Console.ReadLine();
-                try
-                {
-                    NetworkCommsClient.Send(Input);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("发生异常：" + ex);
-                }
-            }
-
-            NetworkCommsServer.Start();
-            Console.ReadLine();
-            Console.ReadLine();
         }
     }
 }
