1f73261ad4ba96d6939d86dafe7de2890321dc95
luchunminglu <luchunminglu@gmail.com>
2017-02-23 11:12:52 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/AppExitEx.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/AppExitEx.cs
@@ -14,7 +14,8 @@ namespace Introduce_To_Algorithm3.Common.Utils
         /// </summary>
         /// <param name="normalExitAction">正常退出执行的动作</param>
         /// <param name="exceptionHandler">异常处理</param>
-        public static void EnsureExit(Action normalExitAction = null,Action<Exception> exceptionHandler = null) 
+        /// <param name="maxWaitMilliSecondBeforeExit">退出之前最长的等待时间，单位毫秒,即normalExitAction可能执行的最长时间，超出该时间说明正常退出指令写的有问题，程序强制杀死</param>
+        public static void EnsureExit(Action normalExitAction = null,Action<Exception> exceptionHandler = null,int maxWaitMilliSecondBeforeExit = 3000) 
         {
             try
             {
@@ -23,7 +24,7 @@ namespace Introduce_To_Algorithm3.Common.Utils
                 Task.Factory.StartNew(() =>
                 {
                     //3s后强制退出
-                    Thread.Sleep(3000);
+                    Thread.Sleep(maxWaitMilliSecondBeforeExit);
                     for (int i = 0; i < 5; i++)
                     {
                         try
