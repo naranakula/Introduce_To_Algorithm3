d737a34813e2bf5a454fd77c0727f652d3d3b689
luchunminglu <luchunminglu@gmail.com>
2017-06-07 15:55:17 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Program.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Program.cs
@@ -7,7 +7,6 @@ namespace Introduce_To_Algorithm3
     {
         public static void Main(string[] args)
         {
-            NLogHelper.Info(CodeCounter.GetCodeLines(@"C:\Users\cmlu\Documents\GitHub\Introduce_To_Algorithm3\Introduce_To_Algorithm3\Introduce_To_Algorithm3") +"");
             NLogHelper.Info(GuidUtils.GetGuid2());
             NLogHelper.Trace("Trace");
             NLogHelper.Debug("Debug");
