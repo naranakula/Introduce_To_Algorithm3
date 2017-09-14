a106cdd5e01570922d84e3e0215a3cd07319d941
luchunminglu <luchunminglu@gmail.com>
2017-09-14 22:37:46 +0800

    cc

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/Redis_/RedisClientHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduce_To_Algorithm3.OpenSourceLib.Redis_
+{
+    /// <summary>
+    /// Redis是开源内存data structure store，可以作为db\cache\message broker
+    /// 在使用Redis之前考虑下CacheHelper
+    /// Redis>memcached
+    /// </summary>
+    public class RedisClientHelper
+    {
+    }
+}
