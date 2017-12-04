e77ad56fcc6fd680e12eb48540ff9f715d06b125
cmlu <luchunminglu@gmail.com>
2017-12-04 16:13:45 +0800

    xx

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/grpcs/examples/exs/compile.bat
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/grpcs/examples/exs/compile.bat
@@ -1,3 +1,8 @@
-windows_x86\protoc.exe -I E:\grpcs --csharp_out E:\grpcs\Greeter --grpc_out E:\grpcs\Greeter2 E:\grpcs\test.proto --plugin=protoc-gen-grpc=E:\grpcs\windows_x86\grpc_csharp_plugin.exe
+cd /d E:\grpcs
+
+windows_x86\protoc.exe -I . --csharp_out . --grpc_out . ElectService.proto HealthCheck.proto Monitor.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe
+
+
+protoc.exe -I E:\grpcs --csharp_out E:\grpcs\Greeter --grpc_out E:\grpcs\Greeter2 E:\grpcs\test.proto --plugin=protoc-gen-grpc=E:\grpcs\windows_x86\grpc_csharp_plugin.exe
 
 pause
