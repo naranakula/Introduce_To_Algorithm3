2deafef243b923f6af7427a2b3dc0f9ea9381ac0
luchunminglu <luchunminglu@gmail.com>
2018-01-20 12:47:41 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/CodeCounter.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/CodeCounter.cs
@@ -14,7 +14,7 @@ namespace Introduce_To_Algorithm3.Common.Utils
         /// <param name="dir"></param>
         /// <param name="pattern"></param>
         /// <returns></returns>
-        public static int GetCodeLines(string dir,string pattern = "*.cs")
+        public static int GetCodeLines(string dir, string pattern = "*.cs")
         {
             DirectoryInfo di = new DirectoryInfo(dir);
             if (!di.Exists)
@@ -27,7 +27,7 @@ namespace Introduce_To_Algorithm3.Common.Utils
             int count = 0;
             foreach (FileInfo fi in fis)
             {
-                using (StreamReader sr = new StreamReader(fi.FullName,Encoding.UTF8))
+                using (StreamReader sr = new StreamReader(fi.FullName, Encoding.UTF8))
                 {
                     while (sr.ReadLine() != null)
                     {
@@ -39,13 +39,17 @@ namespace Introduce_To_Algorithm3.Common.Utils
             return count;
         }
 
+        #region 去掉注释
+
         /// <summary>
-        /// 将代码中的注释去掉
+        /// 将代码中的//注释去掉，并且备份数据
+        /// 仅仅去掉//注释
         /// </summary>
-        /// <param name="dir"></param>
+        /// <param name="dir">目录</param>
         /// <param name="pattern"></param>
-        /// <param name="encodingStr"></param>
-        public static void RemoveComments(string dir,String pattern="*.cs",string encodingStr = "utf-8")
+        /// <param name="encodingStr">编码</param>
+        /// <param name="isBak">是否备份文件</param>
+        public static void RemoveComments(string dir, String pattern = "*.cs", string encodingStr = "utf-8",bool isBak=true)
         {
             Encoding encoding = Encoding.GetEncoding(encodingStr);
             DirectoryInfo dirInfo = new DirectoryInfo(dir);
@@ -57,13 +61,29 @@ namespace Introduce_To_Algorithm3.Common.Utils
             //获取所有文件
             FileInfo[] fileInfos = dirInfo.GetFiles(pattern, SearchOption.AllDirectories);
 
-            foreach(FileInfo fileItem in fileInfos)
+            #region 备份
+            if (isBak)
+            {
+                string datePart = DateTime.Now.ToString("yyyyMMddHHmmss");
+                string extensionPart = "." + datePart + ".bak";
+                foreach (FileInfo fileItem in fileInfos)
+                {
+                    string fullName = fileItem.FullName;
+                    string bakFullName = fullName + extensionPart;
+                    //备份文件
+                    File.Copy(fullName, bakFullName, true);
+                }
+            }
+            #endregion
+
+
+            foreach (FileInfo fileItem in fileInfos)
             {
                 StringBuilder sb = new StringBuilder();
-                using(StreamReader reader = new StreamReader(fileItem.FullName, encoding))
+                using (StreamReader reader = new StreamReader(fileItem.FullName, encoding))
                 {
                     string str = null;
-                    while((str = reader.ReadLine()) != null)
+                    while ((str = reader.ReadLine()) != null)
                     {
                         string strTrim = str.Trim();
                         if (strTrim.StartsWith(@"//") && !strTrim.Contains(@"/*") && !strTrim.Contains(@"*/"))
@@ -79,12 +99,16 @@ namespace Introduce_To_Algorithm3.Common.Utils
                     }
                 }
 
-                using(StreamWriter writer = new StreamWriter(fileItem.FullName, append: false, encoding: encoding))
+                using (StreamWriter writer = new StreamWriter(fileItem.FullName, append: false, encoding: encoding))
                 {
                     writer.Write(sb.ToString());
                 }
             }
 
         }
+
+
+        #endregion
+
     }
 }
