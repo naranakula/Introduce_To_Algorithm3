96230760ae8aa05da57f9920d13090abf875dcf0
cmlu <luchunminglu@gmail.com>
2018-01-25 09:18:09 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/ActiveMq/Producers/MQShortConnectionProducer.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/ActiveMq/Producers/MQShortConnectionProducer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Apache.NMS;
 using Apache.NMS.ActiveMQ;
 using Apache.NMS.ActiveMQ.Commands;
@@ -126,7 +127,323 @@ namespace Introduce_To_Algorithm3.OpenSourceLib.ActiveMq.Producers
         }
 
 
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void SendQueueMessage(byte[] msg, Action<Exception> exceptionHandler = null)
+        {
+            try
+            {
+                using (var connection = factory.CreateConnection())
+                {
+                    //超时16s
+                    connection.RequestTimeout = new TimeSpan(0, 0, 16);
+                    //start应该放在这
+                    connection.Start();
+                    using (var session = connection.CreateSession())
+                    {
+                        using (
+                            var producer =
+                                session.CreateProducer(session.GetDestination(
+                                    destQueueName, //queue的名称 
+                                    DestinationType.Queue)))
+                        {
+                            //start不能放在这
+                            //connection.Start();
+                            IBytesMessage message = session.CreateBytesMessage(msg);
+                            ////The timestamp of when the message was pubished in UTC time. If the publisher disables setting the timestamp on the message, the time will be set to the start of the UNIX epoc (1970-01-01 00:00:00).
+                            //message.NMSTimestamp = DateTime.UtcNow;
+                            ////The amount of time for which this message is valid.
+                            //message.NMSTimeToLive = TimeSpan.FromHours(2);
+                            ////是否持久消息
+                            //message.NMSDeliveryMode = MsgDeliveryMode.Persistent;
+                            //producer.Send(message);
+
+                            producer.Send(message, MsgDeliveryMode.Persistent, MsgPriority.Normal, TimeSpan.FromHours(2));
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (exceptionHandler != null)
+                {
+                    exceptionHandler(ex);
+                }
+            }
+        }
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void SendTopicMessage(byte[] msg, Action<Exception> exceptionHandler = null)
+        {
+            try
+            {
+                using (var connection = factory.CreateConnection())
+                {
+                    //超时16s
+                    connection.RequestTimeout = new TimeSpan(0, 0, 16);
+                    //start应该放在这
+                    connection.Start();
+                    using (var session = connection.CreateSession())
+                    {
+                        using (
+                            var producer =
+                                session.CreateProducer(session.GetDestination(destTopicName,//topic名称
+                                    DestinationType.Topic)))
+                        {
+                            //start不能放在这
+                            //connection.Start();
+                            IBytesMessage message = session.CreateBytesMessage(msg);
+                            ////The timestamp of when the message was pubished in UTC time. If the publisher disables setting the timestamp on the message, the time will be set to the start of the UNIX epoc (1970-01-01 00:00:00).
+                            //message.NMSTimestamp = DateTime.UtcNow;
+                            ////The amount of time for which this message is valid.
+                            //message.NMSTimeToLive = TimeSpan.FromMinutes(20);
+                            ////是否持久消息
+                            //message.NMSDeliveryMode = MsgDeliveryMode.Persistent;
+
+                            producer.Send(message, MsgDeliveryMode.NonPersistent, MsgPriority.Normal, TimeSpan.FromMinutes(30));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (exceptionHandler != null)
+                {
+                    exceptionHandler(ex);
+                }
+            }
+
+        }
+
+
         #region 批量发送
+
+
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="msgList"></param>
+        public static void SendQueueMessage(List<string> msgList, Action<Exception> exceptionHandler = null)
+        {
+            if (msgList == null || msgList.Count == 0)
+            {
+                return;
+            }
+
+            try
+            {
+                using (var connection = factory.CreateConnection())
+                {
+                    //超时16s
+                    connection.RequestTimeout = new TimeSpan(0, 0, 16);
+                    //start应该放在这
+                    connection.Start();
+                    using (var session = connection.CreateSession())
+                    {
+                        using (
+                            var producer =
+                                session.CreateProducer(session.GetDestination(
+                                    destQueueName, //queue的名称 
+                                    DestinationType.Queue)))
+                        {
+                            foreach (string msg in msgList)
+                            {
+                                //start不能放在这
+                                //connection.Start();
+                                ITextMessage message = session.CreateTextMessage(msg);
+                                ////The timestamp of when the message was pubished in UTC time. If the publisher disables setting the timestamp on the message, the time will be set to the start of the UNIX epoc (1970-01-01 00:00:00).
+                                //message.NMSTimestamp = DateTime.UtcNow;
+                                ////The amount of time for which this message is valid.
+                                //message.NMSTimeToLive = TimeSpan.FromHours(2);
+                                ////是否持久消息
+                                //message.NMSDeliveryMode = MsgDeliveryMode.Persistent;
+                                //producer.Send(message);
+
+                                producer.Send(message, MsgDeliveryMode.Persistent, MsgPriority.Normal, TimeSpan.FromHours(2));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (exceptionHandler != null)
+                {
+                    exceptionHandler(ex);
+                }
+            }
+        }
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="msgList"></param>
+        public static void SendTopicMessage(List<string> msgList, Action<Exception> exceptionHandler = null)
+        {
+            if (msgList == null || msgList.Count == 0)
+            {
+                return;
+            }
+
+            try
+            {
+                using (var connection = factory.CreateConnection())
+                {
+                    //超时16s
+                    connection.RequestTimeout = new TimeSpan(0, 0, 16);
+                    //start应该放在这
+                    connection.Start();
+                    using (var session = connection.CreateSession())
+                    {
+                        using (
+                            var producer =
+                                session.CreateProducer(session.GetDestination(destTopicName,//topic名称
+                                    DestinationType.Topic)))
+                        {
+                            foreach (string msg in msgList)
+                            {
+                                //start不能放在这
+                                //connection.Start();
+                                ITextMessage message = session.CreateTextMessage(msg);
+                                ////The timestamp of when the message was pubished in UTC time. If the publisher disables setting the timestamp on the message, the time will be set to the start of the UNIX epoc (1970-01-01 00:00:00).
+                                //message.NMSTimestamp = DateTime.UtcNow;
+                                ////The amount of time for which this message is valid.
+                                //message.NMSTimeToLive = TimeSpan.FromMinutes(20);
+                                ////是否持久消息
+                                //message.NMSDeliveryMode = MsgDeliveryMode.Persistent;
+
+                                producer.Send(message, MsgDeliveryMode.NonPersistent, MsgPriority.Normal,
+                                    TimeSpan.FromMinutes(30));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (exceptionHandler != null)
+                {
+                    exceptionHandler(ex);
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="msgList"></param>
+        public static void SendQueueMessage(List<byte[]> msgList, Action<Exception> exceptionHandler = null)
+        {
+            if (msgList == null || msgList.Count == 0)
+            {
+                return;
+            }
+
+            try
+            {
+                using (var connection = factory.CreateConnection())
+                {
+                    //超时16s
+                    connection.RequestTimeout = new TimeSpan(0, 0, 16);
+                    //start应该放在这
+                    connection.Start();
+                    using (var session = connection.CreateSession())
+                    {
+                        using (
+                            var producer =
+                                session.CreateProducer(session.GetDestination(
+                                    destQueueName, //queue的名称 
+                                    DestinationType.Queue)))
+                        {
+                            foreach (byte[] msg in msgList)
+                            {
+                                //start不能放在这
+                                //connection.Start();
+                                IBytesMessage message = session.CreateBytesMessage(msg);
+                                ////The timestamp of when the message was pubished in UTC time. If the publisher disables setting the timestamp on the message, the time will be set to the start of the UNIX epoc (1970-01-01 00:00:00).
+                                //message.NMSTimestamp = DateTime.UtcNow;
+                                ////The amount of time for which this message is valid.
+                                //message.NMSTimeToLive = TimeSpan.FromHours(2);
+                                ////是否持久消息
+                                //message.NMSDeliveryMode = MsgDeliveryMode.Persistent;
+                                //producer.Send(message);
+
+                                producer.Send(message, MsgDeliveryMode.Persistent, MsgPriority.Normal, TimeSpan.FromHours(2));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (exceptionHandler != null)
+                {
+                    exceptionHandler(ex);
+                }
+            }
+        }
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="msgList"></param>
+        public static void SendTopicMessage(List<byte[]> msgList, Action<Exception> exceptionHandler = null)
+        {
+            if (msgList == null || msgList.Count == 0)
+            {
+                return;
+            }
+
+            try
+            {
+                using (var connection = factory.CreateConnection())
+                {
+                    //超时16s
+                    connection.RequestTimeout = new TimeSpan(0, 0, 16);
+                    //start应该放在这
+                    connection.Start();
+                    using (var session = connection.CreateSession())
+                    {
+                        using (
+                            var producer =
+                                session.CreateProducer(session.GetDestination(destTopicName,//topic名称
+                                    DestinationType.Topic)))
+                        {
+                            foreach (byte[] msg in msgList)
+                            {
+                                //start不能放在这
+                                //connection.Start();
+                                IBytesMessage message = session.CreateBytesMessage(msg);
+                                ////The timestamp of when the message was pubished in UTC time. If the publisher disables setting the timestamp on the message, the time will be set to the start of the UNIX epoc (1970-01-01 00:00:00).
+                                //message.NMSTimestamp = DateTime.UtcNow;
+                                ////The amount of time for which this message is valid.
+                                //message.NMSTimeToLive = TimeSpan.FromMinutes(20);
+                                ////是否持久消息
+                                //message.NMSDeliveryMode = MsgDeliveryMode.Persistent;
+
+                                producer.Send(message, MsgDeliveryMode.NonPersistent, MsgPriority.Normal,
+                                    TimeSpan.FromMinutes(30));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (exceptionHandler != null)
+                {
+                    exceptionHandler(ex);
+                }
+            }
+
+        }
+
+
 
         #endregion
 
