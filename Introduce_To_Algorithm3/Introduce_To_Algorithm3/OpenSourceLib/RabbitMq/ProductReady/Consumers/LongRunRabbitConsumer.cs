91a345301c919cafe3fb7ff719f9d8ad424e4b28
cmlu <luchunminglu@gmail.com>
2018-02-13 13:37:31 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/RabbitMq/ProductReady/Consumers/LongRunRabbitConsumer.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/RabbitMq/ProductReady/Consumers/LongRunRabbitConsumer.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Introduce_To_Algorithm3.Common.Utils;
+using Introduce_To_Algorithm3.OpenSourceLib.Utils;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace Introduce_To_Algorithm3.OpenSourceLib.RabbitMq.ProductReady.Consumers
 {
@@ -37,7 +40,70 @@ namespace Introduce_To_Algorithm3.OpenSourceLib.RabbitMq.ProductReady.Consumers
          * 生产者将消息发送到exchange(类似于邮局),Exchange将消息放到队列.
          * Exchanges distribute message copies to queues using rules called bindings. 
          * 消费者从队列中获取消息.
+         * Exchange类型
+Name	Default pre-declared names
+Direct exchange	(Empty string) and amq.direct
+Fanout exchange	amq.fanout
+Topic exchange	amq.topic
+Headers exchange	amq.match (and amq.headers in RabbitMQ)
+         * Exchange可以是持久的或者短暂的，持久的当机器重启后，仍然存在
+         * |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+         * Direct Exchange
+         * 直接交换根据Routing key将消息放到队列，标准是队列名和Routing key一样
+         * |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+         * Fanout Exchange
+         * Fanout Exchange是发布订阅模型，Fanout Exchange将消息放入所有绑定到它的队列，忽略Routing key.
+         * 在Fanout Exchange中，Routing key不起作用
+         * |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+         * Topic Exchange
+         * Topic Exchange将消息放入0或多个队列，根据Routing key和queue的绑定的pattern.
+         * 绑定的队列可以只获取Topic Exchange的一个子集
+         * Messages sent to a topic exchange can't have an arbitrary routing_key - it must be a list of words, delimited by dots. The words can be anything, but usually they specify some features connected to the message. A few valid routing key examples: "stock.usd.nyse", "nyse.vmw", "quick.orange.rabbit". 
+         * topic消息的routing key必须使用.隔开
+         * topic中 *替换一个单词 #替换0个或多个单词
+         * * (star) can substitute for exactly one word.
+         * # (hash) can substitute for zero or more words.
+         * |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+         * Headers Exchange
+         * Headers Exchange根据Header的一个或者多个attribute来放置消息，而不是根据Routing key
+         * Headers exchanges ignore the routing key attribute. Instead, the attributes used for routing are taken from the headers attribute. 
+         * 可以全部匹配也可以只匹配一个header的attribute
          * 
+         * Queue队列
+         * Queue有如下属性：
+         * name:名字 最长255 utf8字符串;  Durable:持久性  Exclusive:排他性，只能在本连接中使用 Auto-delete:自动删除
+         * Queue declaration will have no effect if the queue does already exist and its attributes are the same as those in the declaration. When the existing queue attributes are not the same as those in the declaration a channel-level exception with code 406 (PRECONDITION_FAILED) will be raised.
+         * Queue names starting with "amq." are reserved for internal use by the broker. Attempts to declare a queue with a name that violates this rule will result in a channel-level exception with reply code 403 (ACCESS_REFUSED).
+         * Durable queues are persisted to disk and thus survive broker restarts.
+         * Durability of a queue does not make messages that are routed to that queue durable.
+         * 
+         * Bindings
+         * Binding定义Exchange route消息到queue的规则。
+         * 
+         * Consumers
+         * 有两种:push 和 pull  Have messages delivered to them ("push API") Fetch messages as needed ("pull API")
+         * 自动通知:当消息发送到客户端时，从queue中删除
+         * 手动通知:当客户端通知时，从queue中删除
+         * 
+         * 消息属性,消息具有如下属性(不限于):
+Content type
+Content encoding
+Routing key
+Delivery mode (persistent or not)
+Message priority
+Message publishing timestamp
+Expiration period
+Publisher application id
+         * 
+         * Connections
+         * 表示一个tcp连接
+         * 
+         * Channels
+         * tcp连接的一个多路复用channel
+         * hannels can be thought of as "lightweight connections that share a single TCP connection".
+         * 
+         * Virtual Hosts
+         * similar to virtual hosts used by many popular Web servers and provide completely isolated environments in which AMQP entities live.
          * 
          * 
          */
@@ -56,24 +122,207 @@ namespace Introduce_To_Algorithm3.OpenSourceLib.RabbitMq.ProductReady.Consumers
 
         /// <summary>
         /// 连接 多线程安全
+        /// 表示一个TCP连接
         /// </summary>
         private static volatile IConnection _connection = null;
 
         /// <summary>
         /// Common AMQP model, the union of the functionality 
-        /// 一个连接下的通道
+        /// 一个tcp连接下的多路复用通道
         /// </summary>
         private static volatile IModel _channel = null;
 
+        #region Exchange类型
+
+
+        /// <summary>
+        /// 默认mq创建的Direct类型的Exchange的名字
+        /// </summary>
+        public const string DefaultDirectExchangeName = "";
+
+
+        /// <summary>
+        /// Direct exchange 类型
+        /// </summary>
+        public const string DirectExchangeTypeName = "direct";
+
+        /// <summary>
+        /// fanout exchange 类型
+        /// </summary>
+        public const string FanoutExchangeTypeName = "fanout";
+
+        /// <summary>
+        /// topic exchange 类型
+        /// </summary>
+        public const string TopicExchangeTypeName = "topic";
+
+        #endregion
+
+
         #region 配置相关
-        
+
         /// <summary>
         /// true表示使用queue,false表示使用主题
         /// </summary>
         private static readonly bool _isQueue = true;
 
+        /// <summary>
+        /// 当是队列时，使用的队列名称
+        /// </summary>
+        private static readonly string _queueName = ConfigUtils.GetString("QueueName");
+
+        /// <summary>
+        /// 当使用fanout时，使用的Exchange的名字
+        /// </summary>
+        private static readonly string _fanoutExchangeName = ConfigUtils.GetString("TopicExchangeName");
 
         #endregion
+
+        /// <summary>
+        /// 开启消费者
+        /// </summary>
+        /// <param name="exceptionHandler"></param>
+        /// <returns></returns>
+        public static bool StartConsumer(Action<Exception> exceptionHandler = null)
+        {
+            try
+            {
+                StopConsumer();
+
+                _connection = SFactory.CreateConnection();
+                //定义连接事件
+                _connection.ConnectionBlocked += ConnectionOnConnectionBlocked;
+
+                //创建多路复用的channel
+                _channel = _connection.CreateModel();
+
+                if (_isQueue)
+                {
+                    //队列
+                    //queue:队列名 
+                    //durable:true if we are declaring a durable queue (the 队列 will survive a server restart) 当mq重启后队列是否仍然存在
+                    //exclusive:排他性，如果为true,表示队列只能被当前连接使用，连接关闭队列自动消失
+                    //autoDelete:当已经没有消费者时，服务器是否可以删除该队列,true if the server should delete the queue when it is no longer in use。
+                    _channel.QueueDeclare(queue: _queueName, durable: true, exclusive: false, autoDelete: false,arguments: null);
+
+                    //prefetchSize - maximum amount of content (measured in octets) that the server will deliver, 0 if unlimited
+                    //prefetchCount - maximum number of messages that the server will deliver, 0 if unlimited
+                    //global - true if the settings should be applied to the entire channel rather than each consumer
+                    //每次取一个消息，消息大小不限制
+                    //注释掉该行，将会使用Robbin轮盘分配，使用下面这行，只有空闲的Consumer接收消息
+                    //_channel.BasicQos(prefetchSize:0,prefetchCount:1,global:false);
+                    
+                    //定义消息接收事件
+                    EventingBasicConsumer consumer = new EventingBasicConsumer(_channel);
+                    consumer.Received += ConsumerOnReceived;
+                    //自动应答
+                    _channel.BasicConsume(queue:_queueName, autoAck:true, consumer:consumer);
+                }
+                else
+                {
+                    // 1、创建exchange
+                    //使用fanout
+                    //exchange:exchange的名字，//type:exchange的类型
+                    //durable - true if we are declaring a durable exchange (the exchange will survive a server restart)
+                    //autoDelete - true if the server should delete the exchange when it is no longer in use
+                    //arguments - other properties (construction arguments) for the exchange
+                    _channel.ExchangeDeclare(exchange:_fanoutExchangeName,type:FanoutExchangeTypeName,durable:true,autoDelete:false,arguments:null);
+
+                    //2、创建queue
+                    //由MQ服务器创建一个amp.开头的(如amq.gen-JzTY20BRgKO-HjmUJj0wLg)非持久、排他、自动删除的队列
+                    //连接断开后，服务器将删除该队列
+                    string tempQueueName = _channel.QueueDeclare().QueueName;
+
+                    //3、绑定queue
+                    //将队列绑定到exchange
+                    //fanout的routingkey不起作用，将会被忽略
+                    //使用指定的Routingkey将指定的queue绑定到指定的exchange上
+                    //如果关心多种消息，一个channel可以创建多个bind，可以使用相同的队列名(一个队列关心多个routingKey)
+                    _channel.QueueBind(queue:tempQueueName,exchange:_fanoutExchangeName,routingKey:"",arguments:null);
+
+                    //定义消息接收事件
+                    EventingBasicConsumer consumer = new EventingBasicConsumer(_channel);
+                    consumer.Received += ConsumerOnReceived;
+                    //自动应答
+                    _channel.BasicConsume(queue: tempQueueName, autoAck: true, consumer: consumer);
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                exceptionHandler?.Invoke(e);
+                return false;
+            }
+        }
+
+        #region 消息接收事件
+
+        /// <summary>
+        /// 消息接收事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private static void ConsumerOnReceived(object sender, BasicDeliverEventArgs args)
+        {
+
+            try
+            {
+                //获取消息
+                byte[] body = args.Body;
+
+
+            }
+            catch (Exception e)
+            {
+                NLogHelper.Error($"mq处理消息失败:{e}");
+            }
+
+        }
+
+        #endregion
+
+        #region 连接事件回调
+
+        private static void ConnectionOnConnectionBlocked(object sender, ConnectionBlockedEventArgs connectionBlockedEventArgs)
+        {
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 停止消费者
+        /// </summary>
+        public static void StopConsumer()
+        {
+
+            if (_channel != null)
+            {
+                SafeInvokeUtils.Safe(() =>
+                {
+                    _channel.Dispose();
+                }, ex =>
+                {
+                    NLogHelper.Error($"关闭RabbitMQ的channel失败:{ex}");
+                });
+
+                _channel = null;
+            }
+
+            if (_connection != null)
+            {
+                SafeInvokeUtils.Safe(() =>
+                {
+                    _connection.Dispose();
+                }, ex =>
+                {
+                    NLogHelper.Error($"关闭RabbitMQ的connection失败:{ex}");
+                });
+                _connection = null;
+            }
+
+        }
+
 
 
     }
--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/RabbitMq/ProductReady/ShortConnectionProducer.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/RabbitMq/ProductReady/ShortConnectionProducer.cs
@@ -28,14 +28,25 @@ namespace Introduce_To_Algorithm3.OpenSourceLib.RabbitMq.ProductReady
         //Why would you want multiple virtual hosts? Easy. A username in RabbitMQ grants you access to a virtual host…in its entirety. So the only way to keep group A from accessing group B’s exchanges/queues/bindings/etc. is to create a virtual host for A and one for B. 
         //Virtualhost是虚拟主机,类似于命名空间
         private static readonly ConnectionFactory SFactory = new ConnectionFactory() { UserName = "guest", Password = "guest", VirtualHost = ConnectionFactory.DefaultVHost, HostName = "192.168.163.14", Port = 5672, AutomaticRecoveryEnabled = false/*不自动重连*/, RequestedConnectionTimeout = ConnectionFactory.DefaultConnectionTimeout, SocketReadTimeout = ConnectionFactory.DefaultConnectionTimeout, SocketWriteTimeout = ConnectionFactory.DefaultConnectionTimeout/*以上三个值就是不设置时使用的默认的值*/};
+        
+        /// <summary>
+        /// 默认mq创建的Direct类型的Exchange的名字
+        /// </summary>
+        public const string DefaultDirectExchangeName = "";
 
         /// <summary>
-        /// 
+        /// Direct Exchange类型
         /// </summary>
-        private const string DefaultExchangeName = "";
+        private const string DirectExchangeTypeName = "direct";
+
+        /// <summary>
+        /// Fanout Exchange
+        /// </summary>
+        private const string FanoutExchangeTypeName = "fanout";
 
         /// <summary>
         /// 发送队列消息
+        /// 发送到direct exchang的默认的""exchange中
         /// </summary>
         /// <param name="queueName"></param>
         /// <param name="messageBytes"></param>
@@ -50,17 +61,71 @@ namespace Introduce_To_Algorithm3.OpenSourceLib.RabbitMq.ProductReady
                 {
                     using (var channel = connection.CreateModel())
                     {
-                        //queue:队列名 durable:true if we are declaring a durable exchange (the exchange will survive a server restart) 
+                        //queue:队列名 
+                        //durable:true if we are declaring a durable 队列 (the 队列 will survive a server restart) 当mq重启后队列是否仍然存在
                         //exclusive:排他性，如果为true,表示队列只能被当前连接使用，连接关闭队列自动消失
-                        //autoDelete:当已经没有消费者时，服务器是否可以删除该Exchange,true if the server should delete the exchange when it is no longer in use。
+                        //autoDelete:当已经没有消费者时，服务器是否可以删除该队列,true if the server should delete the queue when it is no longer in use。
+                        //该queue默认会绑定到direct exchange的默认的实例""上，除非你显式的调用QueueBind
                         channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false,
                             arguments: null);
 
                         IBasicProperties properties = channel.CreateBasicProperties();
+                        //定义消息的持久性
                         properties.Persistent = true;
+
+                        //设置单个消息的过期时间单位毫秒
+                        properties.Expiration = "7200000";//两个小时
+
+                        //Direct Exchange将会把消息发送到和routingkey一样的队列中
+                        //使用指定的routing key发送到指定的exchange
+                        channel.BasicPublish(exchange: DefaultDirectExchangeName, routingKey: queueName, basicProperties: properties, body: messageBytes);
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                exceptionHandler?.Invoke(e);
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// 发送Fanout topic消息
+        /// </summary>
+        /// <param name="exchangeName"></param>
+        /// <param name="messageBytes"></param>
+        /// <param name="exceptionHandler"></param>
+        /// <returns></returns>
+        public static bool SendFanoutTopicMessage(string exchangeName, byte[] messageBytes,
+            Action<Exception> exceptionHandler = null)
+        {
+            try
+            {
+                using (var connection = SFactory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        //使用fanout
+                        //exchange:exchange的名字
+                        //type:exchange的类型
+                        //durable - true if we are declaring a durable exchange (the exchange will survive a server restart)
+                        //autoDelete - true if the server should delete the exchange when it is no longer in use
+                        //arguments - other properties (construction arguments) for the exchange
+                        channel.ExchangeDeclare(exchange: exchangeName, type: FanoutExchangeTypeName, durable: true, autoDelete: false, arguments: null);
+
+
+                        IBasicProperties properties = channel.CreateBasicProperties();
+                        //定义消息的持久性
+                        properties.Persistent = false;
                         //properties.Expiration
 
-                        channel.BasicPublish(exchange: DefaultExchangeName, routingKey: queueName, basicProperties: properties, body: messageBytes);
+                        //当fanout类型，routingKey不起作用，将被忽略
+                        //使用指定的routing key发送到指定的exchange
+                        channel.BasicPublish(exchange: exchangeName, routingKey: "", basicProperties: properties, body: messageBytes);
+
                     }
                 }
 
