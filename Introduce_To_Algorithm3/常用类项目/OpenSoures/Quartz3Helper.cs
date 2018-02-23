deb52bc8879a092c684d50c68bbf3d289133b18c
cmlu <luchunminglu@gmail.com>
2018-02-23 14:09:02 +0800

    cc

---
+++ Introduce_To_Algorithm3/常用类项目/OpenSoures/Quartz3Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 常用类项目.OpenSoures
+{
+    /// <summary>
+    /// Quartz3需要.net4.5.2
+    /// 
+    /// 
+    /// </summary>
+    public static class Quartz3Helper
+    {
+    }
+}
