007741faa8ef8514dea20e11e6fc13f14971b40c
cmlu <luchunminglu@gmail.com>
2018-05-16 15:41:26 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/sqls/EF2/Sqlite/SqliteCodeFirstContext.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/sqls/EF2/Sqlite/SqliteCodeFirstContext.cs
@@ -136,6 +136,18 @@ INTEGER as Unix Time, the number of seconds since 1970-01-01 00:00:00 UTC.
 
 
             modelBuilder.Configurations.Add(new BaseEntityMap());
+
+
+            #region SqliteSqlGenerator
+
+            //var model = modelBuilder.Build(Database.Connection);
+            //ISqlGenerator sqlGenerator = new SqliteSqlGenerator();
+            //string sql = sqlGenerator.Generate(model.StoreModel);
+
+
+            #endregion
+
+
         }
 
         #region 原生SQL调用
