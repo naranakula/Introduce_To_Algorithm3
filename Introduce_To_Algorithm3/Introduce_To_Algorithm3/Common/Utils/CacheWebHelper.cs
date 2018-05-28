856806118e4d31e0b83b37b772aa1ac8e9c8938f
cmlu <luchunminglu@gmail.com>
2018-05-28 11:10:36 +0800

    cc

--- Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/CacheWebHelper.cs
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/CacheWebHelper.cs
@@ -8,17 +8,31 @@ using System.Web.Caching;
 
 namespace Introduce_To_Algorithm3.Common.Utils
 {
+    /// <summary>
+    /// Web中使用的cache
+    /// </summary>
     public static class CacheWebHelper
     {
         /// <summary>
         /// 添加一个缓存项，如果已经存在相同的key,此方法将会覆盖现有值。
+        /// 返回true，表示没有异常
         /// </summary>
         /// <param name="key"></param>
         /// <param name="value"></param>
         /// <param name="expireSeconds">过多少秒后过期</param>
-        public static void Insert(string key, object value, int expireSeconds)
+        public static bool Insert(string key, object value, int expireSeconds,Action<Exception> exceptionHandler = null)
         {
-            HttpRuntime.Cache.Insert(key, value, null, DateTime.UtcNow.AddSeconds(expireSeconds), Cache.NoSlidingExpiration);
+            try
+            {
+                HttpRuntime.Cache.Insert(key, value, null, DateTime.UtcNow.AddSeconds(expireSeconds),
+                    Cache.NoSlidingExpiration);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                exceptionHandler?.Invoke(ex);
+                return false;
+            }
         }
 
         /// <summary>
