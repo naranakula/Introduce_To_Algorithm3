83471d7f7ef60b8b29d54affe59ac0b09912f1fa
cmlu <luchunminglu@gmail.com>
2018-05-31 09:31:13 +0800

    cc

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/Images/ImageUtils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduce_To_Algorithm3.Common.Utils.Images
+{
+
+
+    /// <summary>
+    /// 图片工具
+    /// </summary>
+    public static class ImageUtils
+    {
+
+        /// <summary>
+        /// 字节数组转Image
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static Image BytesToImage(byte[] bytes)
+        {
+            MemoryStream ms = new MemoryStream(bytes);
+            return Image.FromStream(ms);
+        }
+
+
+        /// <summary>
+        /// Image转字节
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        public static byte[] BitmapToBytes(Image bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Jpeg);
+
+                return ms.ToArray();
+            }
+        }
+    }
+
+
+}
