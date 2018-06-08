31a4a709e0315e03d5a08537b6386d62b9849918
cmlu <luchunminglu@gmail.com>
2018-06-08 09:34:58 +0800

    cc

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/sqls/EF2/CommonDbMaps/ConfigItemMap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Introduce_To_Algorithm3.Common.Utils.sqls.EF2.CommonDbModels;
+
+namespace Introduce_To_Algorithm3.Common.Utils.sqls.EF2.CommonDbMaps
+{
+    /// <summary>
+    /// 数据库映射
+    /// </summary>
+    public class ConfigItemMap:EntityTypeConfiguration<ConfigItem>
+    {
+
+        public ConfigItemMap()
+        {
+
+            ToTable(nameof(ConfigItem)).HasKey(p => p.Id);
+            
+            //变长 nvarchar(36)
+            Property(x => x.Id)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(36)
+                .IsVariableLength()
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+
+
+            //nvarchar(128)
+            Property(t => t.ConfigKey).IsRequired().IsUnicode().HasMaxLength(128).IsVariableLength();
+
+
+            //nvarchar(128)
+            Property(t => t.ConfigFor).IsRequired().IsUnicode().HasMaxLength(128).IsVariableLength();
+
+
+            //nvarchar(max)
+            Property(x => x.ConfigValue)
+                .IsOptional()
+                .IsUnicode()
+                .HasMaxLength(null)
+                .IsVariableLength();
+        }
+
+    }
+}
---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/Common/Utils/sqls/EF2/CommonDbModels/ConfigItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduce_To_Algorithm3.Common.Utils.sqls.EF2.CommonDbModels
+{
+    /// <summary>
+    /// 配置项
+    /// </summary>
+    public class ConfigItem
+    {
+
+        /// <summary>
+        /// Id
+        /// </summary>
+        public string Id { get; set; }
+
+
+        /// <summary>
+        /// 配置项键
+        /// </summary>
+        public string ConfigKey { get; set; }
+
+
+        /// <summary>
+        /// 配置项值
+        /// </summary>
+        public string ConfigValue { get; set; }
+
+
+        /// <summary>
+        /// 空表示为所有配置
+        /// 不为空表示为某个指定客户端配置
+        /// 为谁做的配置
+        /// </summary>
+        public string ConfigFor { get; set; }
+
+
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        public DateTime UpdateTime { get; set; }
+
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+    }
+}
