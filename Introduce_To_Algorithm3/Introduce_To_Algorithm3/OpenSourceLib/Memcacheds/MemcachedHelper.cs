bc41d201b6d50ba868885f26a584dfacfab8b65a
cmlu <luchunminglu@gmail.com>
2018-07-19 09:56:29 +0800

    cc

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/Memcacheds/MemcachedHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduce_To_Algorithm3.OpenSourceLib.Memcacheds
+{
+
+    /// <summary>
+    /// 经过综合评估，采用nuget的 enyimmemcached
+    /// 网站:https://github.com/enyim/EnyimMemcached
+    /// 推荐使用Redis，而不是memcached
+    /// </summary>
+    public static class MemcachedHelper
+    {
+    }
+
+
+}
