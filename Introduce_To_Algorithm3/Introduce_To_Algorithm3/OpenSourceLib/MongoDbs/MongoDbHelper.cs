91d49fc1416a89d91cd7419b43f8e78eb1e78fb4
cmlu <luchunminglu@gmail.com>
2018-08-29 13:01:31 +0800

    xx

---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/MongoDbs/MongoDbHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduce_To_Algorithm3.OpenSourceLib.MongoDbs
+{
+
+    /// <summary>
+    /// MongoDb帮助类
+    /// nuget:MongoDB.Driver
+    /// http://mongodb.github.io/mongo-csharp-driver/
+    /// https://docs.mongodb.com/manual/introduction/
+    /// </summary>
+    public static class MongoDbHelper
+    {
+    }
+}
---
+++ Introduce_To_Algorithm3/Introduce_To_Algorithm3/OpenSourceLib/MongoDbs/ReadMe.txt
@@ -0,0 +1,22 @@
+﻿nuget:MongoDB.Driver
+官网: 
+	http://mongodb.github.io/mongo-csharp-driver/
+	https://docs.mongodb.com/manual/introduction/
+
+MongoDB是开源的文档数据库，提供高性能、高可用和 automatic scaling.
+
+A record in mongodb is a document, which is a data structure composed of field and value pair.The values of fields may include other documents, arrays, and arrays of documents.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
